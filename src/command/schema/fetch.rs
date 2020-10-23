2e6a42deee1215f5e6c0723b5909aa416bf00dec
Jake Dawkins <dawkinsjh@gmail.com>
2020-10-23 10:25:15 -0400

    address review comments
    - return from command with error instead of printing to log::error
    - fix graph_name naming
    - fix profile_name for consistency

--- src/command/schema/fetch.rs
+++ src/command/schema/fetch.rs
@@ -7,24 +7,24 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 pub struct Fetch {
     /// ID of the graph to fetch from Apollo Studio
-    #[structopt(name = "SCHEMA_ID")]
-    schema_id: String,
+    #[structopt(name = "GRAPH_NAME")]
+    graph_name: String,
     /// The variant of the request graph from Apollo Studio
     #[structopt(long, default_value = "current")]
     variant: String,
-    #[structopt(long, default_value = "default")]
-    profile: String,
+    #[structopt(long = "profile", default_value = "default")]
+    profile_name: String,
 }
 
 impl Fetch {
     pub fn run(&self) -> Result<()> {
-        match config::Profile::get_api_key(&self.profile) {
+        match config::Profile::get_api_key(&self.profile_name) {
             Ok(api_key) => {
                 log::info!(
                     "Let's get this schema, {}@{}, mx. {}!",
-                    &self.schema_id,
+                    &self.graph_name,
                     &self.variant,
-                    &self.profile
+                    &self.profile_name
                 );
 
                 // TODO (future): move client creation to session
@@ -35,18 +35,14 @@ impl Fetch {
 
                 let schema = get::run(
                     get::get_schema_query::Variables {
-                        graph_id: self.schema_id.clone(),
+                        graph_id: self.graph_name.clone(),
                         hash: None,
                         variant: Some(self.variant.clone()),
                     },
                     client,
-                );
+                )?;
 
-                match schema {
-                    Ok(schema) => log::info!("{}", schema),
-                    Err(err) => log::error!("{}", err),
-                };
-
+                log::info!("{}", schema); 
                 Ok(())
             }
             Err(e) => Err(e),
