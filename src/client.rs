20dccb974510e8f83d382ea5b912d5a287802b00
Jake Dawkins <dawkinsjh@gmail.com>
2020-11-19 15:21:02 -0500

    add APOLLO_REGISTRY_URI to owverwrite endpoint

--- src/client.rs
+++ src/client.rs
@@ -1,12 +1,15 @@
 use anyhow::Result;
 use houston as config;
 use rover_client::blocking::StudioClient;
+use std::env;
 
 const STUDIO_PROD_API_ENDPOINT: &str = "https://graphql.api.apollographql.com/api/graphql";
 
 pub(crate) fn get_studio_client(profile: &str) -> Result<StudioClient> {
     let api_key = config::Profile::get_api_key(profile)?;
-    Ok(StudioClient::new(&api_key, STUDIO_PROD_API_ENDPOINT))
+    let endpoint =
+        env::var("APOLLO_REGISTRY_URI").unwrap_or_else(|_| String::from(STUDIO_PROD_API_ENDPOINT));
+    Ok(StudioClient::new(&api_key, &endpoint))
 }
 
 // pub(crate) fn get_client(uri: &str) -> Result<Client> {
