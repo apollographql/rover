57e4dd57f76e0aba1ceb014ef283b8e092c9f2b7
Avery Harnish <avery@apollographql.com>
2021-01-13 11:32:14 -0600

    fix(npm): use correct repo url (#176)
    Co-authored-by: Jake Dawkins <dawkinsjh@gmail.com>

--- installers/npm/binary.js
+++ installers/npm/binary.js
@@ -56,7 +56,7 @@ const getBinary = () => {
   const platform = getPlatform();
   // the url for this binary is constructed from values in `package.json`
   // https://github.com/apollographql/rover/releases/download/v1.0.0/binary-install-example-v1.0.0-x86_64-apple-darwin.tar.gz
-  const url = `${repository.url}/releases/download/v${version}/${name}-v${version}-${platform.RUST_TARGET}.tar.gz`;
+  const url = `${repository}/releases/download/v${version}/${name}-v${version}-${platform.RUST_TARGET}.tar.gz`;
   return new Binary(platform.BINARY_NAME, url);
 };
 
