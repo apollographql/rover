665b28e1717a40b8277eda4e224079538526cae6
Jake Dawkins <dawkinsjh@gmail.com>
2021-02-01 13:18:51 -0500

    0.0.1-rc.5 (#216)
    * cargo update & build for 0.0.1-rc5
    * update all other versions

--- installers/binstall/scripts/windows/install.ps1
+++ installers/binstall/scripts/windows/install.ps1
@@ -2,7 +2,7 @@ function Install-Binary() {
   $old_erroractionpreference = $ErrorActionPreference
   $ErrorActionPreference = 'stop'
 
-  $version = "0.0.1-rc.4"
+  $version = "0.0.1-rc.5"
 
   Initialize-Environment
 
