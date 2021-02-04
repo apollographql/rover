ca06fb109b899a0f99f55b8fdd498abcb2fe531a
Jake Dawkins <dawkinsjh@gmail.com>
2021-02-04 15:10:31 -0500

    add failing test to debug win/mac

---
+++ tests/info.rs
@@ -0,0 +1,14 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+
+#[test]
+fn it_prints_info() {
+    let mut cmd = Command::cargo_bin("rover").unwrap();
+    let result = cmd.arg("info")
+        .assert()
+        .success();
+    
+    dbg!(&result.get_output());
+
+    result.stdout(predicate::str::contains("Info"));
+}
