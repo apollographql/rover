51d21ff2a75d6e6eda54567e2d4d2b1ffee4cf66
Jake Dawkins <dawkinsjh@gmail.com>
2021-02-04 15:39:04 -0500

    fix integration test

--- tests/info.rs
+++ tests/info.rs
@@ -6,7 +6,7 @@ fn it_prints_info() {
     let mut cmd = Command::cargo_bin("rover").unwrap();
     let result = cmd.arg("info").assert().success();
 
-    println!("{:?}", &result.get_output());
-
-    //result.stdout(predicate::str::contains("Info"));
+    // the version should always be available in the `info` output
+    let version = env!("CARGO_PKG_VERSION");
+    result.stderr(predicate::str::contains(version));
 }
