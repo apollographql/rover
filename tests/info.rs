4cb418ba839a29ab618f427f8108e0eed3292df3
Jake Dawkins <dawkinsjh@gmail.com>
2021-02-04 15:21:01 -0500

    add nocapture

--- tests/info.rs
+++ tests/info.rs
@@ -4,10 +4,8 @@ use predicates::prelude::*;
 #[test]
 fn it_prints_info() {
     let mut cmd = Command::cargo_bin("rover").unwrap();
-    let result = cmd.arg("info")
-        .assert()
-        .success();
-    
+    let result = cmd.arg("info").assert().success();
+
     println!("{:?}", &result.get_output());
 
     //result.stdout(predicate::str::contains("Info"));
