e4ad199b974c2f6cbe536ca386043c06c6901065
Avery Harnish <avery@apollographql.com>
2021-03-09 15:13:55 -0600

    chore: auto bump versions in our windows/nix install scripts

--- build.rs
+++ build.rs
@@ -1,12 +1,13 @@
 use anyhow::{anyhow, Context, Error, Result};
+use camino::{Utf8Path, Utf8PathBuf};
+use regex::bytes::Regex;
+
 use std::{
     env, fs,
     path::PathBuf,
     process::{Command, Output},
     str,
 };
-
-use camino::{Utf8Path, Utf8PathBuf};
 
 /// files to copy from the repo's root directory into the npm tarball
 const FILES_TO_COPY: &[&str; 2] = &["LICENSE", "README.md"];
@@ -24,6 +25,7 @@ fn main() -> Result<()> {
         }
     }
 
+    prep_installer_versions()?;
     prep_npm(is_release_build)
 }
 
@@ -33,6 +35,71 @@ fn rerun_if_changed(filename: &str) {
 
 fn cargo_warn(message: &str) {
     println!("cargo:warn=/!\\ {}", message);
+}
+
+/// prep_installer_versions prepares our curl/iwr installers
+/// with the Cargo.toml version
+fn prep_installer_versions() -> Result<()> {
+    let scripts_dir = get_binstall_scripts_root();
+    prep_nix_installer(&scripts_dir)?;
+    prep_windows_installer(&scripts_dir)
+}
+
+/// prep_nix_installer updates our curl installer with the Cargo.toml version
+fn prep_nix_installer(parent: &Utf8Path) -> Result<()> {
+    let installer = Utf8PathBuf::from(parent).join("nix").join("install.sh");
+    let old_installer_contents = fs::read_to_string(installer.as_path())
+        .context("Could not read contents of nix installer to a String")?;
+    let version_regex = Regex::new(r#"(?:PACKAGE_VERSION="v){1}(.*)"{1}"#)
+        .context("Could not create regular expression for nix installer version replacer")?;
+    let old_version = str::from_utf8(
+        version_regex
+            .captures(old_installer_contents.as_bytes())
+            .expect("Could not find PACKAGE_VERSION in nix/install.sh")
+            .get(1)
+            .expect("Could not find the version capture group in nix/install.sh")
+            .as_bytes(),
+    )
+    .context("Capture group is not valid UTF-8")?;
+    let new_installer_contents = old_installer_contents.replace(old_version, PKG_VERSION);
+    fs::write(installer.as_path(), &new_installer_contents)
+        .context("Could not write updated PACKAGE_VERSION to nix/install.sh")?;
+    Ok(())
+}
+
+/// prep_windows_installer updates our windows installer with the Cargo.toml version
+fn prep_windows_installer(parent: &Utf8Path) -> Result<()> {
+    let installer = Utf8PathBuf::from(parent)
+        .join("windows")
+        .join("install.ps1");
+    let old_installer_contents = fs::read_to_string(installer.as_path())
+        .context("Could not read contents of windows installer to a String")?;
+    let version_regex = Regex::new(r#"(?:\$package_version = 'v){1}(.*)'{1}"#)
+        .context("Could not create regular expression for windows installer version replacer")?;
+    let old_version = str::from_utf8(
+        version_regex
+            .captures(old_installer_contents.as_bytes())
+            .expect("Could not find $package_version in windows/install.ps1")
+            .get(1)
+            .expect("Could not find the version capture group in windows/install.ps1")
+            .as_bytes(),
+    )
+    .context("Capture group is not valid UTF-8")?;
+    let new_installer_contents = old_installer_contents.replace(old_version, PKG_VERSION);
+    fs::write(installer.as_path(), &new_installer_contents)
+        .context("Could not write updated $package_version to windows/install.ps1")?;
+    Ok(())
+}
+
+/// get_binstall_scripts_root gets the parent directory
+/// of our nix/windows install scripts
+fn get_binstall_scripts_root() -> Utf8PathBuf {
+    let root_directory = Utf8PathBuf::new();
+
+    root_directory
+        .join("installers")
+        .join("binstall")
+        .join("scripts")
 }
 
 /// prep_npm prepares our npm installer package for release
@@ -82,7 +149,7 @@ fn prep_npm(is_release_build: bool) -> Result<()> {
         install_dependencies(&npm_install_path, &npm_dir)
             .context("Could not install dependencies.")?;
 
-        update_version(&npm_install_path, &npm_dir)
+        update_npm_version(&npm_install_path, &npm_dir)
             .context("Could not update version in package.json.")?;
 
         if is_release_build {
@@ -132,7 +199,7 @@ fn install_dependencies(npm_install_path: &Utf8Path, npm_dir: &Utf8Path) -> Resu
     process_command_output(&command_output).context("Could not print output of 'npm install'.")
 }
 
-fn update_version(npm_install_path: &Utf8Path, npm_dir: &Utf8Path) -> Result<()> {
+fn update_npm_version(npm_install_path: &Utf8Path, npm_dir: &Utf8Path) -> Result<()> {
     let command_output = Command::new(npm_install_path)
         .current_dir(npm_dir)
         .arg("version")
