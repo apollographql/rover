5181f1a10344a14504245548d589b51843edcb39
Avery Harnish <avery@apollographql.com>
2021-03-17 10:26:41 -0500

    chore: schema->path, service->subgraph

--- src/command/core/build.rs
+++ src/command/core/build.rs
@@ -2,7 +2,7 @@ use crate::{anyhow, command::RoverStdout, Result};
 
 use ansi_term::Colour::Red;
 use camino::Utf8PathBuf;
-use harmonizer::{self, ServiceDefinition};
+use harmonizer::{self, ServiceDefinition as SubgraphDefinition};
 use serde::{Deserialize, Serialize};
 use structopt::StructOpt;
 
@@ -14,40 +14,40 @@ pub struct Build {
     /// The path to the subgraph configuration file.
     #[structopt(long = "config")]
     #[serde(skip_serializing)]
-    subgraph_config: Utf8PathBuf,
+    core_config: Utf8PathBuf,
 }
 
 #[derive(Deserialize)]
-struct SubgraphConfig {
+struct CoreConfig {
     subgraphs: HashMap<String, Subgraph>,
 }
 
 #[derive(Deserialize)]
 struct Subgraph {
     url: String,
-    schema: Utf8PathBuf,
+    path: Utf8PathBuf,
 }
 
 impl Build {
     pub fn run(&self) -> Result<RoverStdout> {
-        let service_list_contents = fs::read_to_string(&self.subgraph_config)?;
-        let parsed_service_list: SubgraphConfig = serde_yaml::from_str(&service_list_contents)?;
-        let mut service_list = Vec::new();
-        for (subgraph_name, subgraph_data) in parsed_service_list.subgraphs {
-            let relative_schema_path = if let Some(parent) = self.subgraph_config.parent() {
+        let raw_core_config = fs::read_to_string(&self.core_config)?;
+        let parsed_core_config: CoreConfig = serde_yaml::from_str(&raw_core_config)?;
+        let mut subgraphs = Vec::new();
+        for (subgraph_name, subgraph_data) in parsed_core_config.subgraphs {
+            let relative_schema_path = if let Some(parent) = self.core_config.parent() {
                 let mut schema_path = parent.to_path_buf();
-                schema_path.push(subgraph_data.schema);
+                schema_path.push(subgraph_data.path);
                 schema_path
             } else {
-                subgraph_data.schema
+                subgraph_data.path
             };
             let schema = fs::read_to_string(&relative_schema_path)?;
-            let service_definition =
-                ServiceDefinition::new(subgraph_name, subgraph_data.url, &schema);
-            service_list.push(service_definition);
+            let subgraph_definition =
+                SubgraphDefinition::new(subgraph_name, subgraph_data.url, &schema);
+            subgraphs.push(subgraph_definition);
         }
 
-        match harmonizer::harmonize(service_list) {
+        match harmonizer::harmonize(subgraphs) {
             Ok(csdl) => Ok(RoverStdout::CSDL(csdl)),
             Err(composition_errors) => {
                 let num_failures = composition_errors.len();
