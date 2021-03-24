ef4a169ad164a6acc89cb85cda9cdf548b2aeca3
Avery Harnish <avery@apollographql.com>
2021-03-24 12:04:15 -0500

    chore: add retries with backoff to schema download

--- crates/rover-client/build.rs
+++ crates/rover-client/build.rs
@@ -1,8 +1,13 @@
 use std::env;
 use std::fs::{self, read, write};
+use std::thread;
+use std::time::Duration;
 
+use anyhow::Result;
 use camino::Utf8PathBuf as PathBuf;
-use reqwest::blocking::Client;
+use reqwest::blocking::{Client, Request, Response};
+use reqwest::{Method, Url};
+
 use uuid::Uuid;
 
 /// This script downloads the schema if it's not in the file system
@@ -13,7 +18,7 @@ use uuid::Uuid;
 /// The URL to fetch the schema can be overriden with the APOLLO_GPAPHQL_SCHEMA_URL environment variable.
 ///
 /// Note: eprintln! statements only show up with `cargo build -vv`
-fn main() -> std::io::Result<()> {
+fn main() -> anyhow::Result<()> {
     // Rerun the build if this script updates last_run.uuid (which it does every time).
     eprintln!("cargo:rerun-if-changed=.schema/last_run.uuid");
     fs::create_dir_all(".schema")?;
@@ -43,10 +48,7 @@ fn main() -> std::io::Result<()> {
             let current_etag = String::from_utf8(read(etag_path)?).unwrap();
             eprintln!("current etag: {}", current_etag);
 
-            let response = client
-                .head(&schema_url)
-                .send()
-                .expect("Failed to get headers from Apollo's schema download url.");
+            let response = retry_request(&client, Method::HEAD, &schema_url)?;
 
             let remote_etag = response.headers().get("etag").and_then(|v| v.to_str().ok());
             eprintln!("remote etag: {}", remote_etag.unwrap_or("None"));
@@ -65,12 +67,38 @@ fn main() -> std::io::Result<()> {
     }
 }
 
-fn update_schema(client: &Client, url: &str) -> std::io::Result<()> {
+fn retry_request(client: &Client, method: Method, url: &str) -> Result<Response> {
+    let mut delay_secs = 2;
+    let max_delay_secs = 30;
+    let mut has_failed = false;
+
+    loop {
+        let parsed_url = Url::parse(url)?;
+        let request = Request::new(method.clone(), parsed_url);
+        if let Ok(response) = client.execute(request) {
+            if has_failed {
+                eprintln!("Success!");
+            }
+            return Ok(response);
+        } else {
+            has_failed = true;
+            eprintln!("failed to fetch {}.", url);
+            eprintln!("retrying connection in {} seconds", delay_secs);
+            thread::sleep(Duration::from_secs(delay_secs));
+            delay_secs = delay_secs.pow(2);
+
+            if delay_secs > max_delay_secs {
+                delay_secs = max_delay_secs;
+            }
+
+            eprintln!("retrying fetch...");
+        }
+    }
+}
+
+fn update_schema(client: &Client, url: &str) -> anyhow::Result<()> {
     eprintln!("Updating schema.");
-    let response = client
-        .get(url)
-        .send()
-        .expect("Failed to get GraphQL schema from Apollo's schema download url.");
+    let response = retry_request(client, Method::GET, url)?;
 
     let etag = response
         .headers()
