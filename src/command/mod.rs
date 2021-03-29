3217fbfcc262d4d810abe2e88b244e5ea64bdd26
Avery Harnish <avery@apollographql.com>
2021-03-29 13:53:12 -0500

    chore(supergraph): cargo fmt

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -8,7 +8,6 @@ mod subgraph;
 mod supergraph;
 mod update;
 
-pub use supergraph::Supergraph;
 pub use config::Config;
 pub use docs::Docs;
 pub use graph::Graph;
@@ -16,4 +15,5 @@ pub use info::Info;
 pub use install::Install;
 pub use output::RoverStdout;
 pub use subgraph::Subgraph;
+pub use supergraph::Supergraph;
 pub use update::Update;
