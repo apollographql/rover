eadeff52dcfdc479cc829f82ced3540933024553
Jake Dawkins <dawkinsjh@gmail.com>
2021-04-15 13:00:06 -0400

    add some meaningless codes real quik

--- src/error/metadata/code.rs
+++ src/error/metadata/code.rs
@@ -2,7 +2,36 @@ use std::fmt::{self, Display};
 
 /// `Code` contains the error codes associated with specific errors.
 #[derive(Debug)]
-pub enum Code {}
+pub enum Code {
+    E001,
+    E002,
+    E003,
+    E004,
+    E005,
+    E006,
+    E007,
+    E008,
+    E009,
+    E010,
+    E011,
+    E012,
+    E013,
+    E014,
+    E015,
+    E016,
+    E017,
+    E018,
+    E019,
+    E020,
+    E021,
+    E022,
+    E023,
+    E024,
+    E025,
+    E026,
+    E027,
+    E028,
+}
 
 impl Display for Code {
     fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
