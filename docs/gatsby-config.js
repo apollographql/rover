45cd14b8d3ed24a3a4311d74f4d52821e6d44f1c
Jake Dawkins <dawkinsjh@gmail.com>
2021-05-05 08:35:25 -0400

    build error reference in build.rs

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
           ],
           'Base Commands': ['graphs'],
           'Federation Commands': ['subgraphs', 'supergraphs'],
+          Reference: ['errors'],
         },
       },
     },
