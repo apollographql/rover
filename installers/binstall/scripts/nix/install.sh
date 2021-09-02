5594375d129c26dd49fdc33339ad0ca64d452e25
Avery Harnish <averyharnish@gmail.com>
2021-09-02 13:34:45 -0500

    fixup: broken link

--- installers/binstall/scripts/nix/install.sh
+++ installers/binstall/scripts/nix/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Licensed under the MIT license
-# <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+# <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
 # option. This file may not be copied, modified, or distributed
 # except according to those terms.
 
