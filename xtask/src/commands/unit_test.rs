d97c60c696dc2648ca77e7f314e4d047735e3978
Avery Harnish <avery@apollographql.com>
2021-09-14 12:54:26 -0500

    chore(ci): run tests with an older glibc (#800)
    * chore(ci): test docker images
    
    * try without dind
    
    * put checkout back where it was
    
    * dont duplicate executor names
    
    * install build-essential
    
    * install pkg-config
    
    * chore(ci): test rover's glibc version
    
    * wip: add glibc checker to tests
    
    * wip: only check glibc in CI
    
    * wip: add xenial image to matrix
    
    * try centos image
    
    * try centos:7
    
    * chore(ci): use yum for centos packages
    
    * chore(ci): add -y to yum commands
    
    * chore(ci): fix up bad find and replace
    
    * chore(ci): install cmake for centos
    
    * chore(ci): install gcc instead of cmake
    
    * chore(ci): install openssl on centos
    
    * chore(ci): groupinstall dev tools centos
    
    * chore(ci): use ubuntu for lint
    
    * chore(ci): persist target directory for integration tests
    
    * chore(ci): persist correct directory
    
    * chore(ci): dont persist directory, it's slow
    
    * chore(ci): run unit tests and integration tests on ubuntu
    
    * chore(ci): run e2e tests in the same job

--- xtask/src/commands/unit_test.rs
+++ xtask/src/commands/unit_test.rs
@@ -1,8 +1,12 @@
 use anyhow::Result;
+use camino::Utf8PathBuf;
 use structopt::StructOpt;
 
 use crate::target::{Target, POSSIBLE_TARGETS};
-use crate::tools::CargoRunner;
+use crate::tools::{CargoRunner, Runner};
+use crate::utils::PKG_PROJECT_ROOT;
+
+use std::{env, str::FromStr};
 
 #[derive(Debug, StructOpt)]
 pub struct UnitTest {
@@ -15,6 +19,20 @@ impl UnitTest {
     pub fn run(&self, verbose: bool) -> Result<()> {
         let mut cargo_runner = CargoRunner::new(verbose)?;
         cargo_runner.test(&self.target)?;
+
+        if let Target::GnuLinux = self.target {
+            if env::var_os("CHECK_GLIBC").is_some() {
+                let check_glibc_script = "./check_glibc.sh".to_string();
+                let runner = Runner {
+                    verbose,
+                    tool_name: check_glibc_script.clone(),
+                    tool_exe: Utf8PathBuf::from_str(&check_glibc_script)?,
+                };
+                let bin_path = format!("./target/{}/debug/rover", &self.target);
+                runner.exec(&[&bin_path], &PKG_PROJECT_ROOT, None)?;
+            }
+        }
+
         Ok(())
     }
 }
