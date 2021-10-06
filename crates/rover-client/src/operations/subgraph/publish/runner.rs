2df3a460e191abd5146c20b0953379aec2f0d2dd
Avery Harnish <averyharnish@gmail.com>
2021-10-06 14:32:34 -0500

    fix: don't require --convert for new variants

--- crates/rover-client/src/operations/subgraph/publish/runner.rs
+++ crates/rover-client/src/operations/subgraph/publish/runner.rs
@@ -1,6 +1,10 @@
 use super::types::*;
 use crate::blocking::StudioClient;
-use crate::operations::config::is_federated::{self, IsFederatedInput};
+use crate::operations::graph::variant::VariantListInput;
+use crate::operations::{
+    config::is_federated::{self, IsFederatedInput},
+    graph::variant,
+};
 use crate::shared::{BuildError, BuildErrors, GraphRef};
 use crate::RoverClientError;
 use graphql_client::*;
@@ -29,18 +33,38 @@ pub fn run(
     // Error here if no --convert flag is passed _and_ the current context
     // is non-federated. Add a suggestion to require a --convert flag.
     if !input.convert_to_federated_graph {
-        let is_federated = is_federated::run(
-            IsFederatedInput {
+        // first, check if the variant exists _at all_
+        // if it doesn't exist, there is no graph schema to "convert"
+        // so don't require --convert in this case, just publish the subgraph
+        let variant_exists = variant::run(
+            VariantListInput {
                 graph_ref: graph_ref.clone(),
             },
             client,
-        )?;
+        )
+        .is_ok();
 
-        if !is_federated {
-            return Err(RoverClientError::ExpectedFederatedGraph {
-                graph_ref,
-                can_operation_convert: true,
-            });
+        if variant_exists {
+            // check if subgraphs have ever been published to this graph ref
+            let is_federated = is_federated::run(
+                IsFederatedInput {
+                    graph_ref: graph_ref.clone(),
+                },
+                client,
+            )?;
+
+            if !is_federated {
+                return Err(RoverClientError::ExpectedFederatedGraph {
+                    graph_ref,
+                    can_operation_convert: true,
+                });
+            }
+        } else {
+            tracing::debug!(
+                "Publishing new subgraph {} to {}",
+                &input.subgraph,
+                &input.graph_ref
+            );
         }
     }
     let data = client.post::<SubgraphPublishMutation>(variables)?;
