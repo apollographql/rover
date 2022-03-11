4d805a00e234b4cfd9ef6af568cb05f2d3acbb66
Avery Harnish <averyharnish@gmail.com>
2022-03-11 11:58:35 -0600

    release: v0.4.5

--- installers/binstall/scripts/nix/install.sh
+++ installers/binstall/scripts/nix/install.sh
@@ -20,7 +20,7 @@ BINARY_DOWNLOAD_PREFIX="https://github.com/apollographql/rover/releases/download
 # Rover version defined in root cargo.toml
 # Note: this line is built automatically
 # in build.rs. Don't touch it!
-PACKAGE_VERSION="v0.4.4"
+PACKAGE_VERSION="v0.4.5"
 
 download_binary_and_run_installer() {
     downloader --check
