f877c9ea0ce7bd98e0157ef2e940d7ea19fd2e91
avery <avery@apollographql.com>
2022-07-26 18:09:38 -0500

    adds example

---
+++ examples/dev/hello/index.js
@@ -0,0 +1,29 @@
+const { ApolloServer, gql } = require('apollo-server');
+const { buildSubgraphSchema} = require("@apollo/subgraph")
+
+// The GraphQL schema
+const typeDefs = gql`
+  type Query {
+    "A simple type for getting started!"
+    hello: String
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  },
+};
+
+const server = new ApolloServer({
+ schema: buildSubgraphSchema({
+   typeDefs,
+   resolvers,
+ })
+});
+
+server.listen({ port: 4001 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
+
