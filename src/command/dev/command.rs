b381cad759264946af79fd51af72123d5ec9196c
avery <avery@apollographql.com>
2022-08-11 17:28:32 -0500

    wip: better process cleanup

--- src/command/dev/command.rs
+++ src/command/dev/command.rs
@@ -1,18 +1,20 @@
 use std::{
     collections::HashMap,
     net::SocketAddr,
-    process::{Child, Command, Stdio},
+    process::{Command, Stdio},
+    sync::mpsc::{channel, sync_channel, Receiver, Sender, SyncSender},
     time::{Duration, Instant},
 };
 
+use command_group::{CommandGroup, GroupChild};
 use dialoguer::Select;
 use rayon::{iter::ParallelIterator, prelude::IntoParallelRefIterator};
 use reqwest::blocking::Client;
 use saucer::{anyhow, Context};
-use sysinfo::{Pid, PidExt, System, SystemExt};
 
 use crate::{
     command::dev::{
+        do_dev::log_err_and_continue,
         netstat::{get_all_local_graphql_endpoints_except, get_all_local_sockets_except},
         socket::{MessageSender, SubgraphName, SubgraphUrl},
     },
@@ -20,23 +22,124 @@ use crate::{
     Result, Suggestion,
 };
 
-#[derive(Debug)]
 pub struct CommandRunner {
-    message_sender: MessageSender,
-    tasks: HashMap<SubgraphName, BackgroundTask>,
-    system: System,
+    background_task_runner: BackgroundTaskRunner,
+    command_message_receiver: Receiver<CommandRunnerMessage>,
 }
 
 impl CommandRunner {
-    pub fn new(socket_addr: &str) -> Self {
+    pub fn new(
+        socket_addr: &str,
+        command_message_receiver: Receiver<CommandRunnerMessage>,
+    ) -> Self {
         Self {
-            message_sender: MessageSender::new(socket_addr),
-            tasks: HashMap::new(),
-            system: System::new(),
+            background_task_runner: BackgroundTaskRunner::new(socket_addr),
+            command_message_receiver,
         }
     }
 
-    pub fn spawn(&mut self, subgraph_name: &SubgraphName, command: &str) -> Result<()> {
+    pub fn message_channel() -> (Sender<CommandRunnerMessage>, Receiver<CommandRunnerMessage>) {
+        channel()
+    }
+
+    pub fn url_channel() -> (
+        SyncSender<Result<SubgraphUrl>>,
+        Receiver<Result<SubgraphUrl>>,
+    ) {
+        sync_channel(1)
+    }
+
+    pub fn ready_channel() -> (SyncSender<()>, Receiver<()>) {
+        sync_channel(1)
+    }
+
+    pub fn handle_command_runner_messages(&mut self) -> ! {
+        loop {
+            match self.command_message_receiver.recv().unwrap() {
+                CommandRunnerMessage::SpawnTaskAndFindUrl {
+                    subgraph_name,
+                    command,
+                    client,
+                    preexisting_socket_addrs,
+                    url_sender,
+                } => {
+                    let url_result = self.background_task_runner.spawn_and_find_url(
+                        subgraph_name,
+                        command,
+                        client,
+                        &preexisting_socket_addrs,
+                    );
+                    url_sender.send(url_result).unwrap();
+                }
+                CommandRunnerMessage::SpawnTask {
+                    subgraph_name,
+                    command,
+                    ready_sender,
+                } => {
+                    let _ = self
+                        .background_task_runner
+                        .spawn(&subgraph_name, &command)
+                        .map_err(log_err_and_continue);
+                    ready_sender.send(()).unwrap()
+                }
+                CommandRunnerMessage::KillTask {
+                    subgraph_name,
+                    ready_sender,
+                } => {
+                    self.background_task_runner.kill_task(&subgraph_name);
+                    ready_sender.send(()).unwrap();
+                }
+                CommandRunnerMessage::KillTasks { ready_sender } => {
+                    self.background_task_runner.kill_tasks();
+                    ready_sender.send(()).unwrap();
+                }
+            };
+        }
+    }
+}
+
+pub enum CommandRunnerMessage {
+    SpawnTaskAndFindUrl {
+        subgraph_name: SubgraphName,
+        command: String,
+        client: Client,
+        preexisting_socket_addrs: Vec<SocketAddr>,
+        url_sender: SyncSender<Result<SubgraphUrl>>,
+    },
+    SpawnTask {
+        subgraph_name: SubgraphName,
+        command: String,
+        ready_sender: SyncSender<()>,
+    },
+    KillTask {
+        subgraph_name: SubgraphName,
+        ready_sender: SyncSender<()>,
+    },
+    KillTasks {
+        ready_sender: SyncSender<()>,
+    },
+}
+
+#[derive(Debug)]
+struct BackgroundTaskRunner {
+    message_sender: MessageSender,
+    tasks: HashMap<SubgraphName, BackgroundTask>,
+}
+
+impl BackgroundTaskRunner {
+    fn new(socket_addr: &str) -> Self {
+        Self {
+            message_sender: MessageSender::new(socket_addr),
+            tasks: HashMap::new(),
+        }
+    }
+
+    fn spawn(&mut self, subgraph_name: &SubgraphName, command: &str) -> Result<()> {
+        tracing::info!(
+            "spawning command '{}' for subgraph '{}'",
+            command,
+            subgraph_name
+        );
         for existing_name in self.tasks.keys() {
             if subgraph_name == existing_name {
                 return Err(RoverError::new(anyhow!(
@@ -56,16 +159,14 @@ impl CommandRunner {
             let mut command = Command::new(bin);
             command.args(args);
             let process = BackgroundTask::new(command)?;
-            let pid = process.pid();
             self.tasks.insert(subgraph_name.to_string(), process);
-            self.system.refresh_process(pid);
             Ok(())
         } else {
             Err(anyhow!("{} is not installed on this machine", &bin).into())
         }
     }
 
-    pub fn spawn_and_find_url(
+    fn spawn_and_find_url(
         &mut self,
         subgraph_name: SubgraphName,
         command: String,
@@ -77,11 +178,12 @@ impl CommandRunner {
         self.spawn(&subgraph_name, &command)?;
         let mut new_graphql_endpoint = None;
         let now = Instant::now();
-        let timeout_secs = 5;
+        let timeout_secs = 120;
         let mut err = RoverError::new(anyhow!(
             "could not find a new GraphQL endpoint for this `rover dev` session after {} seconds",
             timeout_secs
         ));
+        eprintln!("searching for running GraphQL servers... to skip this step, pass the `--url <SUBGRAPH_URL>` argument");
         while new_graphql_endpoint.is_none() && now.elapsed() < Duration::from_secs(timeout_secs) {
             let graphql_endpoints =
                 get_all_local_graphql_endpoints_except(client.clone(), &preexisting_socket_addrs);
@@ -114,6 +216,7 @@ impl CommandRunner {
             std::thread::sleep(Duration::from_secs(1));
         }
         if let Some(graphql_endpoint) = new_graphql_endpoint {
+            eprintln!("detected GraphQL server at {}", &graphql_endpoint);
             Ok(graphql_endpoint)
         } else {
             self.kill_task(&subgraph_name);
@@ -124,16 +227,18 @@ impl CommandRunner {
         }
     }
 
-    pub fn remove_subgraph_message(&self, subgraph_name: &SubgraphName) {
+    fn remove_subgraph_message(&self, subgraph_name: &SubgraphName) {
         let _ = self.message_sender.remove_subgraph(subgraph_name);
     }
 
-    pub fn kill_task(&mut self, subgraph_name: &SubgraphName) {
+    fn kill_task(&mut self, subgraph_name: &SubgraphName) {
+        tracing::info!("killing spawned task for subgraph '{}'", subgraph_name);
         self.remove_subgraph_message(subgraph_name);
         self.tasks.remove(subgraph_name);
     }
 
-    pub fn kill_tasks(&mut self) {
+    fn kill_tasks(&mut self) {
+        tracing::info!("killing all tasks");
         let subgraphs: Vec<&SubgraphName> = self.tasks.keys().collect();
         subgraphs
             .par_iter()
@@ -145,7 +250,7 @@ impl CommandRunner {
 
 #[derive(Debug)]
 struct BackgroundTask {
-    child: Child,
+    child: GroupChild,
 }
 
 impl BackgroundTask {
@@ -154,45 +259,26 @@ impl BackgroundTask {
             command.stdout(Stdio::null()).stderr(Stdio::null());
         }
         let child = command
-            .spawn()
+            .group_spawn()
             .with_context(|| "could not spawn child process")?;
         Ok(Self { child })
     }
 
-    fn pid(&self) -> Pid {
-        Pid::from_u32(self.child.id())
-    }
-
     fn kill(&mut self) {
-        let pid = self.child.id();
-        #[cfg(unix)]
-        {
-            // attempt to stop gracefully
-            unsafe {
-                libc::kill(libc::pid_t::from_ne_bytes(pid.to_ne_bytes()), libc::SIGTERM);
-            }
-
-            for _ in 0..10 {
-                if self.child.try_wait().ok().flatten().is_some() {
-                    break;
-                }
-                std::thread::sleep(std::time::Duration::from_secs(1));
-            }
-        }
-
-        if self.child.try_wait().ok().flatten().is_none() {
-            // still alive? kill it with fire
-            let _ = self.child.kill();
-        }
-
-        if self.child.try_wait().ok().flatten().is_none() {
-            eprintln!("warn: could not kill process with PID '{}'", pid);
-        }
+        let pgid = self.child.id();
+        tracing::info!("killing child with pgid {}", &pgid);
+        let _ = self.child.kill().map_err(|_| {
+            log_err_and_continue(RoverError::new(anyhow!(
+                "could not kill child group with pgid {}",
+                &pgid
+            )));
+        });
     }
 }
 
 impl Drop for BackgroundTask {
     fn drop(&mut self) {
+        tracing::info!("background task with pgid {} was dropped", &self.child.id());
         self.kill()
     }
 }
