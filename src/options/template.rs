600107abcb87f4606c197740ea16f1ed9a37b74f
Dylan Anthony <dbanty@users.noreply.github.com>
2022-09-07 15:43:36 -0600

    chore: Refactor some code to remove some potential panics and integrate enums a bit better.

--- src/options/template.rs
+++ src/options/template.rs
@@ -1,32 +1,31 @@
-use crate::anyhow;
 use saucer::{clap, Parser};
 use serde::{Deserialize, Serialize};
 use std::fmt;
-use std::str::FromStr;
+use std::fmt::Display;
 
 #[derive(Debug, Clone, Serialize, Deserialize, Parser)]
 pub struct TemplateOpt {
-    /// The GitHub id for the templates repository
+    /// The ID for the official template to use
     #[clap(short = 't', long = "template")]
     #[serde(skip_serializing)]
     pub template: Option<String>,
 
     /// Filter templates by the available language
-    #[clap(long = "language")]
+    #[clap(long = "language", value_enum)]
     #[serde(skip_serializing)]
-    pub language: Option<String>,
+    pub language: Option<ProjectLanguage>,
 
     /// Type of template project: client or subgraph
-    #[clap(long = "project-type")]
+    #[clap(long = "project-type", value_enum)]
     #[serde(skip_serializing)]
-    pub project_type: Option<String>,
+    pub project_type: Option<ProjectType>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Parser)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct GithubTemplate {
-    pub id: String,
-    pub git_url: String,
-    pub display: String,
+    pub id: &'static str,
+    pub git_url: &'static str,
+    pub display: &'static str,
     pub language: ProjectLanguage,
     pub project_type: ProjectType,
 }
@@ -42,55 +41,22 @@ impl fmt::Display for GithubTemplate {
     }
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, Parser)]
+#[derive(Clone, Copy, Deserialize, Debug, Eq, PartialEq, Serialize, clap::ValueEnum)]
 pub enum ProjectType {
-    All,
     Subgraph,
     Client,
 }
 
-impl PartialEq<String> for ProjectType {
-    fn eq(&self, other: &String) -> bool {
-        match &self {
-            ProjectType::All => other.to_lowercase() == "all",
-            ProjectType::Subgraph => other.to_lowercase() == "subgraph",
-            ProjectType::Client => other.to_lowercase() == "client",
-        }
-    }
-}
-impl PartialEq for ProjectType {
-    fn eq(&self, other: &ProjectType) -> bool {
-        match &self {
-            ProjectType::All => other.eq(&String::from("all")),
-            ProjectType::Subgraph => other.eq(&String::from("subgraph")),
-            ProjectType::Client => other.eq(&String::from("client")),
-        }
-    }
-}
-impl fmt::Display for ProjectType {
+impl Display for ProjectType {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            ProjectType::All => write!(f, "all"),
             ProjectType::Subgraph => write!(f, "subgraph"),
             ProjectType::Client => write!(f, "client"),
         }
     }
 }
 
-impl FromStr for ProjectType {
-    type Err = saucer::Error;
-
-    fn from_str(input: &str) -> std::result::Result<Self, Self::Err> {
-        match input.to_lowercase().as_str() {
-            "all" => Ok(ProjectType::All),
-            "subgraph" => Ok(ProjectType::Subgraph),
-            "client" => Ok(ProjectType::Client),
-            _ => Err(anyhow!("Invalid Project Type")),
-        }
-    }
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize, Parser)]
+#[derive(Clone, Copy, Debug, Deserialize, Eq, PartialEq, Serialize, clap::ValueEnum)]
 pub enum ProjectLanguage {
     Java,
     Javascript,
@@ -99,29 +65,7 @@ pub enum ProjectLanguage {
     Typescript,
 }
 
-impl PartialEq<String> for ProjectLanguage {
-    fn eq(&self, other: &String) -> bool {
-        match &self {
-            ProjectLanguage::Java => other.to_lowercase() == "java",
-            ProjectLanguage::Javascript => other.to_lowercase() == "javascript",
-            ProjectLanguage::Python => other.to_lowercase() == "python",
-            ProjectLanguage::Rust => other.to_lowercase() == "rust",
-            ProjectLanguage::Typescript => other.to_lowercase() == "typescript",
-        }
-    }
-}
-impl PartialEq for ProjectLanguage {
-    fn eq(&self, other: &ProjectLanguage) -> bool {
-        match &self {
-            ProjectLanguage::Java => other.eq(&String::from("java")),
-            ProjectLanguage::Javascript => other.eq(&String::from("javascript")),
-            ProjectLanguage::Python => other.eq(&String::from("python")),
-            ProjectLanguage::Rust => other.eq(&String::from("rust")),
-            ProjectLanguage::Typescript => other.eq(&String::from("typescript")),
-        }
-    }
-}
-impl fmt::Display for ProjectLanguage {
+impl Display for ProjectLanguage {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             ProjectLanguage::Java => write!(f, "java"),
@@ -129,21 +73,6 @@ impl fmt::Display for ProjectLanguage {
             ProjectLanguage::Python => write!(f, "python"),
             ProjectLanguage::Rust => write!(f, "rust"),
             ProjectLanguage::Typescript => write!(f, "typescript"),
-        }
-    }
-}
-
-impl FromStr for ProjectLanguage {
-    type Err = saucer::Error;
-
-    fn from_str(input: &str) -> std::result::Result<Self, Self::Err> {
-        match input.to_lowercase().as_str() {
-            "java" => Ok(ProjectLanguage::Java),
-            "javascript" => Ok(ProjectLanguage::Javascript),
-            "python" => Ok(ProjectLanguage::Python),
-            "rust" => Ok(ProjectLanguage::Rust),
-            "typescript" => Ok(ProjectLanguage::Typescript),
-            _ => Err(anyhow!("Invalid Project Type")),
         }
     }
 }
