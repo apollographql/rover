55f9d4ea97cf39e0f3b13fd1f6683a86c56d799f
avery <avery@apollographql.com>
2022-09-12 11:55:21 -0500

    chore: s/launchpad/introspector-gadget

--- crates/rover-client/src/blocking/mod.rs
+++ crates/rover-client/src/blocking/mod.rs
@@ -1,6 +1,6 @@
 mod studio_client;
 
-pub use launchpad::blocking::GraphQLClient;
+pub use introspector_gadget::blocking::GraphQLClient;
 pub use studio_client::StudioClient;
 
 pub(crate) const CLIENT_NAME: &str = "rover-client";
