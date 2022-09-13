27a69cedf0d1062b5ec005da71e7e4f50876d5c2
Dylan Anthony <dbanty@users.noreply.github.com>
2022-09-13 17:08:42 -0600

    feat: Execute `post_clone_hook`, if any, during `rover template use`

--- src/command/template/mod.rs
+++ src/command/template/mod.rs
@@ -1,12 +1,16 @@
+use calm_io::stdoutln;
+use std::collections::HashMap;
 use std::convert::TryFrom;
-use std::fs::read_dir;
+use std::fs::{read_dir, read_to_string, remove_file};
 use std::io::Write;
+use std::path::Path;
 
 use console::Term;
 use dialoguer::Select;
+use execute::{shell, Execute};
 use saucer::{clap, Parser};
 use saucer::{Utf8PathBuf, ValueEnum};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 use crate::options::{GithubTemplate, ProjectLanguage, TemplateOpt};
 use crate::utils::client::StudioClientConfig;
@@ -153,6 +157,9 @@ impl UseTemplate {
             &self.path,
             &client_config.get_reqwest_client()?,
         )?;
+
+        run_post_clone_hook(&self.path)?;
+
         Ok(RoverOutput::TemplateUseSuccess {
             template: template_to_clone,
             path: self.path.clone(),
@@ -196,8 +203,8 @@ impl UseTemplate {
         let tarball_path = download_dir_path.join(format!("{}.tar.gz", git_repo_slug));
         let tarball_url = format!("{}/archive/refs/heads/main.tar.gz", template.git_url);
         let mut f = std::fs::File::create(&tarball_path)?;
-        eprintln!("Downloading {}", template.git_url);
-        eprintln!("\tfrom {}", tarball_url);
+        eprintln!("\nDownloading {}", template.git_url);
+        eprintln!("\tfrom {}\n", tarball_url);
         let response_bytes = client
             .get(tarball_url)
             .header(reqwest::header::USER_AGENT, "rover-client")
@@ -234,5 +241,56 @@ impl UseTemplate {
             .into_iter()
             .find(|template| template.id == id)
             .ok_or_else(|| RoverError::new(anyhow!("No template found with id {}", id)))
+    }
+}
+
+/// The `template.yaml` file that is optionally included in a template.
+#[derive(Deserialize)]
+struct TemplateConfig {
+    dependencies: Option<HashMap<String, Dependency>>,
+    post_clone_hook: String,
+}
+
+/// A dependency that must be installed in order to run the `post_clone_hook`
+#[derive(Deserialize)]
+struct Dependency {
+    verify_command: String,
+    failure_message: String,
+}
+
+/// Run the `post_clone_hook`, if any, that the template has configured.
+fn run_post_clone_hook(template_path: &str) -> Result<()> {
+    let template_path = Path::new(template_path);
+    let config_path = template_path.join("template.yaml");
+    if !config_path.is_file() {
+        return Ok(());
+    }
+    let config: TemplateConfig = serde_yaml::from_str(&read_to_string(&config_path)?)?;
+
+    for (_name, dependency) in config.dependencies.iter().flatten() {
+        if dependency.verify_command.is_empty() {
+            continue;
+        }
+        let mut command = shell(&dependency.verify_command);
+        if command.execute_check_exit_status_code(0).is_err() {
+            eprintln!("{}", dependency.failure_message);
+            return Err(RoverError::new(anyhow!("The template was cloned, but the postCloneHook could not be run due to a missing dependency")));
+        }
+    }
+
+    if config.post_clone_hook.is_empty() {
+        return Ok(());
+    }
+    stdoutln!("Running post-clone hook...\n")?;
+    // let mut command = shell(config.post_clone_hook);
+    let mut command = shell(r#"python3 -c 'x = input("what is x? "); print(x)' "#);
+    command.current_dir(template_path);
+    match command.execute_output() {
+        Ok(output) if output.status.success() => {
+            stdoutln!("Removing template.yaml\n\n")?;
+            remove_file(config_path).ok();
+            Ok(())
+        }
+        _ => Err(anyhow!("The template was cloned, but the postCloneHook could not be run").into()),
     }
 }
