d779297e311ab1818c10bf2ddea0569a67bb0ce3
avery <avery@apollographql.com>
2022-09-14 12:57:23 -0500

    chore: removes tests

--- src/utils/emoji.rs
+++ src/utils/emoji.rs
@@ -1,9 +1,8 @@
 use std::fmt::Display;
 
 use console::Emoji as ConsoleEmoji;
-use strum_macros::EnumIter;
 
-#[derive(Debug, Copy, Clone, EnumIter)]
+#[derive(Debug, Copy, Clone)]
 pub enum Emoji {
     Person,
     Web,
@@ -27,19 +26,6 @@ impl Display for Emoji {
             Ok(())
         } else {
             write!(f, "{}", ConsoleEmoji::new(self.get(), ""))
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use strum::IntoEnumIterator;
-
-    #[test]
-    fn each_emoji_has_width_of_three() {
-        for emoji in Emoji::iter() {
-            assert_eq!(console::measure_text_width(&emoji.to_string()), 3)
         }
     }
 }
