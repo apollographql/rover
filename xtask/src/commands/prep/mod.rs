b5e456e72c8f99ff8dce356f1dad31abb2b57556
avery <avery@apollographql.com>
2022-11-17 15:01:12 -0600

    chore: moves schema fetch from build to xtask

--- xtask/src/commands/prep/mod.rs
+++ xtask/src/commands/prep/mod.rs
@@ -1,5 +1,6 @@
 mod docs;
 mod installers;
+mod schema;
 
 use anyhow::{Context, Result};
 use clap::Parser;
@@ -8,23 +9,31 @@ use crate::commands::prep::docs::DocsRunner;
 use crate::tools::{CargoRunner, NpmRunner};
 
 #[derive(Debug, Parser)]
-pub struct Prep {}
+pub struct Prep {
+    #[arg(long = "schema-only")]
+    schema_only: bool,
+}
 
 impl Prep {
     pub fn run(&self, verbose: bool) -> Result<()> {
-        let npm_runner = NpmRunner::new(verbose)?;
-        npm_runner.prepare_package()?;
-        npm_runner.update_linter()?;
-        let cargo_runner = CargoRunner::new(verbose)?;
-        cargo_runner.update_deps()?;
-        installers::update_versions()?;
-        let docs_runner = DocsRunner::new()?;
-        docs_runner
-            .build_error_code_reference()
-            .with_context(|| "Could not build error code reference")?;
-        docs_runner
-            .copy_contributing()
-            .with_context(|| "Could not update contributing.md in the docs.")?;
+        schema::update()?;
+
+        if !self.schema_only {
+            let npm_runner = NpmRunner::new(verbose)?;
+            npm_runner.prepare_package()?;
+            npm_runner.update_linter()?;
+            let cargo_runner = CargoRunner::new(verbose)?;
+            cargo_runner.update_deps()?;
+            installers::update_versions()?;
+            let docs_runner = DocsRunner::new()?;
+            docs_runner
+                .build_error_code_reference()
+                .with_context(|| "Could not build error code reference")?;
+            docs_runner
+                .copy_contributing()
+                .with_context(|| "Could not update contributing.md in the docs.")?;
+        }
+
         Ok(())
     }
 }
--- xtask/src/commands/prep/schema.rs
+++ xtask/src/commands/prep/schema.rs
@@ -1,10 +1,12 @@
-use std::fs::{self, read, write};
+use std::fs;
 
 use anyhow::{anyhow, Result};
 use camino::Utf8PathBuf;
 use reqwest::blocking::Client;
 use rover_std::Fs;
 use uuid::Uuid;
+
+const SCHEMA_DIR: &str = "./crates/rover-client/.schema";
 
 /// This script downloads the schema if it's not in the file system
 /// or if we can detect the user is internet connected.
@@ -13,28 +15,30 @@ use uuid::Uuid;
 ///
 /// The URL to fetch the schema can be overridden with the APOLLO_GPAPHQL_SCHEMA_URL environment variable.
 ///
-/// Note: eprintln! statements only show up with `cargo build -vv`
-fn main() -> Result<()> {
-    Fs::create_dir_all(".schema")?;
+/// Note: crate::info! statements only show up with `cargo build -vv`
+pub fn update() -> Result<()> {
+    let schema_dir = Utf8PathBuf::try_from(SCHEMA_DIR)?;
+    Fs::create_dir_all(&schema_dir)?;
+    Fs::write_file(schema_dir.join(".gitattributes"), "* binary")?;
     let last_run_uuid = Uuid::new_v4().to_string();
-    Fs::write_file(".schema/last_run.uuid", &last_run_uuid)?;
+    Fs::write_file(schema_dir.join("last_run.uuid"), &last_run_uuid)?;
 
-    let hash_path = Utf8PathBuf::from(".schema/hash.id");
+    let hash_path = schema_dir.join("hash.id");
 
     // skip updating the schema if we already have an etag or we're offline
     let should_update_schema = !(hash_path.exists()) || online::check(None).is_ok();
 
     if should_update_schema {
         if !(hash_path.exists()) {
-            eprintln!(".schema/hash.id doesn't exist");
+            crate::info!("{} doesn't exist", &hash_path);
         } else {
-            eprintln!(".schema/hash.id already exists");
-            let current_hash = String::from_utf8(read(hash_path)?).unwrap();
-            eprintln!("current hash: {}", current_hash);
+            crate::info!("{} already exists", &hash_path);
+            let current_hash = Fs::read_file(hash_path)?;
+            crate::info!("current hash: {}", current_hash);
             let remote_hash = query_hash()?;
 
             if remote_hash == current_hash {
-                eprintln!("hashes match. Not updating schema.");
+                crate::info!("hashes match. not updating schema.");
                 return Ok(());
             }
         }
@@ -56,18 +60,16 @@ fn query_schema_and_hash() -> Result<(String, String)> {
 }
 
 fn update_schema(hash: &str, schema: &str) -> Result<()> {
-    eprintln!("Updating schema.");
+    let schema_dir = Utf8PathBuf::try_from(SCHEMA_DIR)?;
 
-    eprintln!("Saving {} to .schema/hash.id", hash);
-    write(".schema/hash.id", hash)?;
+    let hash_path = schema_dir.join("hash.id");
 
-    eprintln!("Writing schema text to .schema/schema.graphql");
-
-    write(".schema/schema.graphql", schema)?;
+    Fs::write_file(hash_path, hash)?;
+    Fs::write_file(schema_dir.join("schema.graphql"), schema)?;
 
     // old versions of Rover wrote to etag.id, since this is no longer needed,
     // let's remove it from dev machines
-    let _ = fs::remove_file("./.schema/etag.id");
+    let _ = fs::remove_file(schema_dir.join("etag.id"));
 
     Ok(())
 }
@@ -88,6 +90,17 @@ const QUERY: &str = r#"query FetchSchema($fetchDocument: Boolean!) {
 fn query(fetch_document: bool) -> Result<(String, Option<String>)> {
     let graphql_endpoint = option_env!("APOLLO_GRAPHQL_SCHEMA_URL")
         .unwrap_or_else(|| "https://api.apollographql.com/api/graphql");
+    if fetch_document {
+        crate::info!(
+            "fetching the latest schema via {}: graph.variant.latestPublication.schema.document...",
+            &graphql_endpoint
+        );
+    } else {
+        crate::info!(
+            "fetching the latest hash via {}: graph.variant.latestPublication.schema.hash...",
+            &graphql_endpoint
+        );
+    }
     let client = Client::new();
     let schema_query = serde_json::json!({
         "variables": {"fetchDocument": fetch_document},
@@ -109,6 +122,7 @@ fn query(fetch_document: bool) -> Result<(String, Option<String>)> {
     }
     let result = &json["data"]["graph"]["variant"]["latestPublication"]["schema"];
     let hash = result["hash"].as_str().unwrap().to_string();
+    crate::info!(" latest hash: {}", &hash);
     let maybe_document = result["document"].as_str().map(|s| s.to_string());
     Ok((hash, maybe_document))
 }
