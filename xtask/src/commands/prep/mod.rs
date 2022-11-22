c47bf1eb241fe24c1d602cefc2ccf4f8329e8c70
avery <avery@apollographql.com>
2022-11-22 11:11:05 -0600

    chore: early return if schema early

--- xtask/src/commands/prep/mod.rs
+++ xtask/src/commands/prep/mod.rs
@@ -18,21 +18,23 @@ impl Prep {
     pub fn run(&self, verbose: bool) -> Result<()> {
         schema::update()?;
 
-        if !self.schema_only {
-            let npm_runner = NpmRunner::new(verbose)?;
-            npm_runner.prepare_package()?;
-            npm_runner.update_linter()?;
-            let cargo_runner = CargoRunner::new(verbose)?;
-            cargo_runner.update_deps()?;
-            installers::update_versions()?;
-            let docs_runner = DocsRunner::new()?;
-            docs_runner
-                .build_error_code_reference()
-                .with_context(|| "Could not build error code reference")?;
-            docs_runner
-                .copy_contributing()
-                .with_context(|| "Could not update contributing.md in the docs.")?;
+        if self.schema_only {
+            return Ok(());
         }
+
+        let npm_runner = NpmRunner::new(verbose)?;
+        npm_runner.prepare_package()?;
+        npm_runner.update_linter()?;
+        let cargo_runner = CargoRunner::new(verbose)?;
+        cargo_runner.update_deps()?;
+        installers::update_versions()?;
+        let docs_runner = DocsRunner::new()?;
+        docs_runner
+            .build_error_code_reference()
+            .with_context(|| "Could not build error code reference")?;
+        docs_runner
+            .copy_contributing()
+            .with_context(|| "Could not update contributing.md in the docs.")?;
 
         Ok(())
     }
