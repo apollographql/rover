088e71c963bd309fdb8d0a1e32df545eaa7adf7e
Avery Harnish <avery@apollographql.com>
2022-11-30 11:23:09 -0600

    chore: updates example to apollo server 4 and remove old example

---
+++ examples/supergraph-demo/pandas/pandas.mjs
@@ -0,0 +1,30 @@
+import { buildSubgraphSchema } from '@apollo/subgraph';
+import { readFileSync } from 'fs';
+import { gql } from 'graphql-tag';
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+const typeDefs = gql(readFileSync('./pandas.graphql', { encoding: 'utf-8' }).toString());
+
+const pandas = [
+  { name: 'Basi', favoriteFood: "bamboo leaves" },
+  { name: 'Yun', favoriteFood: "apple" }
+]
+
+const resolvers = {
+  Query: {
+    allPandas: (_, args, context) => {
+      return pandas;
+    },
+    panda: (_, args, context) => {
+      return pandas.find(p => p.id == args.id);
+    }
+  },
+}
+
+const server = new ApolloServer({
+  schema: buildSubgraphSchema({ typeDefs, resolvers })
+});
+
+const { url } = await startStandaloneServer(server, { listen: { port: 4003 } });
+console.log(`🚀 Pandas server ready at ${url}`);
