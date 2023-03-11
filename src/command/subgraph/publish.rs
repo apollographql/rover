3a2e50c75b4f53cca24063fc36ee37dab527e949
Trevor Scheer <trevor.scheer@gmail.com>
2023-03-10 16:33:53 -0800

    Initial implementation

--- src/command/subgraph/publish.rs
+++ src/command/subgraph/publish.rs
@@ -1,11 +1,15 @@
 use clap::Parser;
+use rover_std::prompt::prompt_confirm_default_no;
 use serde::Serialize;
+use url::{ParseError, Url};
 
 use crate::options::{GraphRefOpt, ProfileOpt, SchemaOpt, SubgraphOpt};
 use crate::utils::client::StudioClientConfig;
 use crate::{RoverOutput, RoverResult};
 
+use rover_client::operations::subgraph::fetch::{self, SubgraphFetchInput};
 use rover_client::operations::subgraph::publish::{self, SubgraphPublishInput};
+
 use rover_client::shared::GitContext;
 use rover_std::Style;
 
@@ -32,8 +36,12 @@ pub struct Publish {
     /// (often a deployed subgraph). May be left empty ("") or a placeholder url
     /// if not running a gateway or router in managed federation mode
     #[arg(long)]
-    #[serde(skip_serializing)]
     routing_url: Option<String>,
+    /// Url of a running subgraph that a supergraph can route operations to
+    /// (often a deployed subgraph). May be left empty ("") or a placeholder url
+    /// if not running a gateway or router in managed federation mode
+    #[arg(long, requires("routing-url"))]
+    allow_invalid_routing_url: bool,
 }
 
 impl Publish {
@@ -42,7 +50,41 @@ impl Publish {
         client_config: StudioClientConfig,
         git_context: GitContext,
     ) -> RoverResult<RoverOutput> {
+        // if a --routing-url is provided AND unparseable AND
+        // --allow-invalid-routing-url is not provided, we need to make some
+        // decisions, otherwise we can assume a publish
+        if self.routing_url.is_some()
+            && Url::parse(&self.routing_url.as_ref().unwrap()).is_err()
+            && !self.allow_invalid_routing_url
+        {
+            if let Some(result) = Self::warn_maybe_prompt() {
+                return Ok(result);
+            }
+        }
+
         let client = client_config.get_authenticated_client(&self.profile)?;
+
+        if self.routing_url.is_none() {
+            let fetch_response = fetch::run(
+                SubgraphFetchInput {
+                    graph_ref: self.graph.graph_ref.clone(),
+                    subgraph_name: self.subgraph.subgraph_name.clone(),
+                },
+                &client,
+            )?;
+
+            if let rover_client::shared::SdlType::Subgraph {
+                routing_url: Some(graph_registry_routing_url),
+            } = fetch_response.sdl.r#type
+            {
+                if let Err(_err) = Url::parse(&graph_registry_routing_url) {
+                    if let Some(result) = Self::warn_maybe_prompt() {
+                        return Ok(result);
+                    }
+                }
+            }
+        }
+
         eprintln!(
             "Publishing SDL to {} (subgraph: {}) using credentials from the {} profile.",
             Style::Link.paint(&self.graph.graph_ref.to_string()),
@@ -74,4 +116,49 @@ impl Publish {
             publish_response,
         })
     }
+
+    pub fn warn_maybe_prompt() -> Option<RoverOutput> {
+        // if we're in a tty, prompt the user
+        if atty::is(atty::Stream::Stdout) {
+            match prompt_confirm_default_no(
+                "Found an invalid URL, would you still like to publish? [y/N]: ",
+            ) {
+                Ok(response) => {
+                    if response {
+                        return None;
+                    } else {
+                        eprintln!("Publish cancelled by user");
+                        return Some(RoverOutput::EmptySuccess);
+                    }
+                }
+                Err(e) => {
+                    return Some(RoverOutput::ErrorExplanation(e.to_string()));
+                }
+            }
+        } else {
+            // if we're not in a tty, we can't prompt. let's print a warning but publish anyway.
+            println!(
+                "{} Found an invalid URL, but we can't prompt in a non-interactive environment. Publishing anyway.",
+                Style::WarningPrefix.paint("WARN:")
+            );
+            return None;
+        }
+    }
 }
+
+// FIXME: remove this
+// old prompt code
+// if atty::is(atty::Stream::Stdout) {
+//     if !prompt_confirm_default_no(
+//         "Found an invalid URL, would you still like to publish? [y/N]: ",
+//     )? {
+//         eprintln!("Publish cancelled by user");
+//         return Ok(RoverOutput::EmptySuccess);
+//     }
+// } else {
+//     // if we're not in a tty, we can't prompt. let's print a warning but publish anyway.
+//     println!(
+//         "{} Found an invalid URL, but we can't prompt in a non-interactive environment. Publishing anyway.",
+//         Style::WarningPrefix.paint("WARN:")
+//     );
+// }
