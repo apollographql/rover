ec9b6565903fa126ca70df7759703892751e7d32
avery <avery@apollographql.com>
2023-06-28 12:53:45 -0500

    fix: use notarytool instead of altool to codesign macos binaries

--- xtask/src/tools/mod.rs
+++ xtask/src/tools/mod.rs
@@ -4,11 +4,17 @@ mod make;
 mod npm;
 mod runner;
 
+#[cfg(target_os = "macos")]
+mod xcrun;
+
 pub(crate) use cargo::CargoRunner;
 pub(crate) use git::GitRunner;
 pub(crate) use make::MakeRunner;
 pub(crate) use npm::NpmRunner;
 pub(crate) use runner::Runner;
+
+#[cfg(target_os = "macos")]
+pub(crate) use xcrun::XcrunRunner;
 
 #[cfg(not(windows))]
 mod lychee;
