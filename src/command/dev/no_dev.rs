1f43923bb0023552f670fa413f8edacae66055be
Nihar Shah <nihar@nihar.io>
2024-04-10 14:11:12 -0400

    feat: enable using the output flat to create supergraph from rover dev

--- src/command/dev/no_dev.rs
+++ src/command/dev/no_dev.rs
@@ -1,4 +1,5 @@
 use super::Dev;
+use crate::options::OutputOpts;
 use crate::{utils::client::StudioClientConfig, RoverError, RoverOutput, RoverResult};
 use anyhow::anyhow;
 use camino::Utf8PathBuf;
@@ -8,6 +9,7 @@ impl Dev {
         &self,
         _override_install_path: Option<Utf8PathBuf>,
         _client_config: StudioClientConfig,
+        _output_opts: &OutputOpts,
     ) -> RoverResult<RoverOutput> {
         Err(RoverError::new(anyhow!(
             "rover dev is not supported on this platform"
