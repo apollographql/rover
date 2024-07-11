4bbf36b769dcdb53f812147baf8c1954ffc7846f
Dylan Anthony <dylan@apollographql.com>
2024-07-11 11:57:22 -0600

    Update to latest `apollo-language-server-core` interface

--- src/command/lsp/mod.rs
+++ src/command/lsp/mod.rs
@@ -1,5 +1,6 @@
 use apollo_language_server_core::server::ApolloLanguageServer;
 use clap::Parser;
+use futures::{channel::mpsc::channel, StreamExt};
 use serde::Serialize;
 use tokio::runtime::Runtime;
 use tower_lsp::{LspService, Server};
@@ -20,7 +21,14 @@ impl Lsp {
 async fn run_lsp() {
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
-    let (service, socket) = LspService::new(ApolloLanguageServer::new);
+    let (sender, mut receiver) = channel(1);
+    let (service, socket) = LspService::new(|client| ApolloLanguageServer::new(client, sender));
     let server = Server::new(stdin, stdout, socket);
+    tokio::spawn(async move {
+        while let Some(definitions) = receiver.next().await {
+            // TODO: run composition
+            tracing::info!("Received message: {:?}", definitions);
+        }
+    });
     server.serve(service).await;
 }
