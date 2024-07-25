ae69a95bbc64a75e9e868a1b750d2cf919629828
jonathanrainer <jonathan.rainer@apollographql.com>
2024-07-25 07:39:30 +0100

    ROVER-69 Optimise --graph-ref uses
    Rather than fetching each subgraph one at a time,
    make a single GraphQL request to get them all at
    once.
    
    This combines the work from #1985

---
+++ crates/rover-client/src/operations/subgraph/fetch_all/types.rs
@@ -0,0 +1,31 @@
+use buildstructor::Builder;
+use derive_getters::Getters;
+
+use crate::shared::GraphRef;
+
+use super::runner::subgraph_fetch_all_query;
+
+pub(crate) type SubgraphFetchAllResponseData = subgraph_fetch_all_query::ResponseData;
+pub(crate) type SubgraphFetchAllGraphVariant =
+    subgraph_fetch_all_query::SubgraphFetchAllQueryVariant;
+pub(crate) type QueryVariables = subgraph_fetch_all_query::Variables;
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct SubgraphFetchAllInput {
+    pub graph_ref: GraphRef,
+}
+
+impl From<SubgraphFetchAllInput> for QueryVariables {
+    fn from(input: SubgraphFetchAllInput) -> Self {
+        Self {
+            graph_ref: input.graph_ref.to_string(),
+        }
+    }
+}
+
+#[derive(Clone, Builder, Debug, Eq, Getters, PartialEq)]
+pub struct Subgraph {
+    name: String,
+    url: Option<String>,
+    sdl: String,
+}
