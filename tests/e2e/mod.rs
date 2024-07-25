0b888b58facfa954da152b4a3671580b817385ea
Jonathan Rainer <Jonathan.rainer@apollographql.com>
2024-07-25 13:58:41 +0100

    Integrate the Smoke Tests Into Integration Test Framework To Allow Easier Extension (#1999)
    This PR converts our existing smoke tests to not be run from `xtask` but
    instead using `rstest` as part of the integration testing framework.
    This makes it easier to extend in future and allows us to use all of
    rust's built in assertion libraries etc.

---
+++ tests/e2e/mod.rs
@@ -0,0 +1,122 @@
+use std::collections::HashMap;
+use std::process::Command;
+use std::time::Duration;
+
+use anyhow::Error;
+use camino::Utf8PathBuf;
+use duct::cmd;
+use git2::Repository;
+use reqwest::Client;
+use rstest::*;
+use serde::Deserialize;
+use serde_json::json;
+use tempfile::TempDir;
+use tokio::time::timeout;
+
+mod dev;
+
+const GRAPHQL_TIMEOUT_DURATION: Duration = Duration::from_secs(30);
+
+#[derive(Debug, Deserialize)]
+struct ReducedSuperGraphConfig {
+    subgraphs: HashMap<String, ReducedSubgraphConfig>,
+}
+#[derive(Debug, Deserialize)]
+struct ReducedSubgraphConfig {
+    routing_url: String,
+}
+
+impl ReducedSuperGraphConfig {
+    pub fn get_subgraph_urls(self) -> Vec<String> {
+        self.subgraphs
+            .values()
+            .map(|x| x.routing_url.clone())
+            .collect()
+    }
+}
+
+#[fixture]
+#[once]
+fn run_subgraphs_retail_supergraph() -> TempDir {
+    println!("Cloning required git repository");
+    // Clone the Git Repository that's needed to a temporary folder
+    let cloned_dir = TempDir::new().expect("Could not create temporary directory");
+    Repository::clone(
+        "https://github.com/apollosolutions/retail-supergraph",
+        cloned_dir.path(),
+    )
+    .expect("Could not clone supergraph repository");
+    // Jump into that temporary folder and run npm commands to kick off subgraphs
+    println!("Installing subgraph dependencies");
+    cmd!("npm", "install")
+        .dir(cloned_dir.path())
+        .run()
+        .expect("Could not install subgraph dependencies");
+    cmd!("npm", "install", "-g", "nodemon")
+        .dir(cloned_dir.path())
+        .run()
+        .expect("Could not install nodemon");
+    println!("Kicking off subgraphs");
+    let mut cmd = Command::new("npx");
+    cmd.env("NODE_ENV", "dev");
+    cmd.args(["nodemon", "index.js"]).current_dir(&cloned_dir);
+    cmd.spawn().expect("Could not spawn subgraph process");
+    println!("Finding subgraph URLs");
+    let subgraph_urls = get_subgraph_urls(
+        Utf8PathBuf::from_path_buf(cloned_dir.path().join("supergraph-config-dev.yaml"))
+            .expect("Could not create path to config"),
+    );
+    println!("Testing subgraph connectivity");
+    for subgraph_url in subgraph_urls {
+        tokio::task::block_in_place(|| {
+            let client = Client::new();
+            let handle = tokio::runtime::Handle::current();
+            handle.block_on(test_graphql_connection(
+                &client,
+                &subgraph_url,
+                GRAPHQL_TIMEOUT_DURATION,
+            ))
+        })
+        .expect("Could not execute connectivity check");
+    }
+    // Return the folder the subgraphs are in
+    cloned_dir
+}
+
+async fn test_graphql_connection(
+    client: &Client,
+    url: &str,
+    timeout_duration: Duration,
+) -> Result<(), Error> {
+    let introspection_query = json!({"query": "{__schema{types{name}}}"});
+    // Loop until we get a response, but timeout if it takes too long
+    timeout(timeout_duration, async {
+        loop {
+            match client.post(url).json(&introspection_query).send().await {
+                Ok(res) => {
+                    if res.status().is_success() {
+                        break;
+                    }
+                }
+                Err(e) => {
+                    println!(
+                        "Could not connect to GraphQL process on {}: {:} - Will retry",
+                        url, e
+                    );
+                }
+            }
+            tokio::time::sleep(Duration::from_secs(2)).await;
+        }
+    })
+    .await?;
+    println!("Established connection to {}", url);
+    Ok(())
+}
+
+fn get_subgraph_urls(supergraph_yaml_path: Utf8PathBuf) -> Vec<String> {
+    let content = std::fs::read_to_string(supergraph_yaml_path)
+        .expect("Could not read supergraph schema file");
+    let sc_config: ReducedSuperGraphConfig =
+        serde_yaml::from_str(&content).expect("Could not parse supergraph schema file");
+    sc_config.get_subgraph_urls()
+}
