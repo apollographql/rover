9ec8135fdf6da10954886a8340c663130e498cbe
jonathanrainer <jonathan.rainer@apollographql.com>
2024-08-01 13:32:08 +0100

    ROVER-78 Add test for `subgraph fetch`

--- tests/e2e/mod.rs
+++ tests/e2e/mod.rs
@@ -157,8 +157,19 @@ async fn test_graphql_connection(
     Ok(())
 }
 
+
 fn get_supergraph_config(supergraph_yaml_path: PathBuf) -> ReducedSupergraphConfig {
     let content = std::fs::read_to_string(supergraph_yaml_path)
         .expect("Could not read supergraph schema file");
     serde_yaml::from_str(&content).expect("Could not parse supergraph schema file")
+
+#[fixture]
+fn remote_supergraph_graphref() -> String {
+    String::from("rover-e2e-tests")
+}
+#[fixture]
+fn test_artifacts_directory() -> PathBuf {
+    let cargo_manifest_dir =
+        env::var("CARGO_MANIFEST_DIR").expect("Could not find CARGO_MANIFEST_DIR");
+   PathBuf::from(cargo_manifest_dir).join("tests/e2e/artifacts")
 }
