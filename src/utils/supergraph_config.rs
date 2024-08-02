61e13942a3bcf523c95d357b54e435cbe92cc6fa
Aaron Arinder <aaronarinder@gmail.com>
2024-08-02 12:34:10 -0400

    fix(tests): add retry_period

--- src/utils/supergraph_config.rs
+++ src/utils/supergraph_config.rs
@@ -120,6 +120,7 @@ mod test_get_supergraph_config {
     use std::io::Write;
     use std::path::PathBuf;
     use std::str::FromStr;
+    use std::time::Duration;
 
     use apollo_federation_types::config::FederationVersion;
     use camino::Utf8PathBuf;
@@ -306,6 +307,7 @@ mod test_get_supergraph_config {
             config,
             false,
             ClientBuilder::default(),
+            Some(Duration::from_secs(3)),
         );
 
         let actual_result = if let Some(name) = local_subgraph {
@@ -635,6 +637,7 @@ mod test_resolve_supergraph_yaml {
     use std::io::Write;
     use std::path::PathBuf;
     use std::string::ToString;
+    use std::time::Duration;
 
     use anyhow::Result;
     use apollo_federation_types::config::{FederationVersion, SchemaSource, SubgraphConfig};
@@ -687,7 +690,13 @@ mod test_resolve_supergraph_yaml {
 
     #[fixture]
     fn client_config(config: Config) -> StudioClientConfig {
-        StudioClientConfig::new(None, config, false, ClientBuilder::default())
+        StudioClientConfig::new(
+            None,
+            config,
+            false,
+            ClientBuilder::default(),
+            Some(Duration::from_secs(3)),
+        )
     }
 
     #[fixture]
@@ -1079,6 +1088,7 @@ type _Service {\n  sdl: String\n}"#;
             config,
             false,
             ClientBuilder::default(),
+            Some(Duration::from_secs(3)),
         );
 
         let mut supergraph_config_path = tempfile::NamedTempFile::new()?;
