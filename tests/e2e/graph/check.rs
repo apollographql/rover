4f379e4a7e234b6578f40851bb71c498f8e9e8ab
Aaron <aaronarinder@gmail.com>
2024-08-05 11:41:11 -0400

    feat(test): e2e graph check, subgraph check (#2025)

---
+++ tests/e2e/graph/check.rs
@@ -0,0 +1,48 @@
+use std::path::PathBuf;
+use std::process::Command;
+
+use assert_cmd::prelude::CommandCargoExt;
+use regex::Regex;
+use rstest::rstest;
+use speculoos::assert_that;
+use speculoos::boolean::BooleanAssertions;
+use tracing_test::traced_test;
+
+use crate::e2e::{remote_supergraph_graphref, test_artifacts_directory};
+
+#[rstest]
+#[ignore]
+#[tokio::test(flavor = "multi_thread")]
+#[traced_test]
+async fn e2e_test_rover_graph_check(
+    remote_supergraph_graphref: String,
+    test_artifacts_directory: PathBuf,
+) {
+    // GIVEN
+    //   - a local schema
+    let supergraph_schema = test_artifacts_directory.join("perfSubgraph00.graphql");
+    let supergraph_schema = supergraph_schema
+        .to_str()
+        .expect("Couldn't get subgraph schema for graph check");
+
+    // WHEN
+    //   - the command is run
+    let mut cmd = Command::cargo_bin("rover").expect("Could not find necessary binary");
+    cmd.args([
+        "graph",
+        "check",
+        "--schema",
+        supergraph_schema,
+        &remote_supergraph_graphref,
+    ]);
+    let output = cmd.output().expect("Could not run command");
+
+    // THEN
+    //   - it matches a regex showing that it linted and passed, meaning a check was performed
+    //   against the remote schema using the local one
+    let stdout = std::str::from_utf8(&output.stdout).expect("failed to convert bytes to a str");
+    let re = Regex::new(r"Linter Check \[PASSED\]").unwrap();
+    let schema_lint_passed = re.is_match(stdout);
+
+    assert_that!(schema_lint_passed).is_true();
+}
--- tests/e2e/subgraph/mod.rs
+++ tests/e2e/subgraph/mod.rs
@@ -1,3 +1,4 @@
+mod check;
 mod fetch;
 mod introspect;
 mod lint;
