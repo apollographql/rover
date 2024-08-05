a2fa6fc06bcd1217f51f82f6b2919a538af19f60
Aaron Arinder <aaronarinder@gmail.com>
2024-08-05 12:52:00 -0400

    feat(tests): install latest config versions from file

--- tests/e2e/install/plugin.rs
+++ tests/e2e/install/plugin.rs
@@ -130,27 +130,45 @@ async fn e2e_test_rover_install_plugin_with_force_opt(
 }
 
 #[rstest]
+#[case::router_latest_1("router", "latest", "latest-1")]
+#[case::supergraph_latest_0("supergraph", "latest-0", "latest-0")]
+#[case::supergraph_latest_2("supergraph", "latest-2", "latest-2")]
+#[ignore]
 #[tokio::test(flavor = "multi_thread")]
 #[traced_test]
-async fn e2e_test_rover_install_plugins_from_latest_plugin_config_file() {
-    // GIVEN
-    //   - a install command for the supergraph binary that forces replacement; sometimes this
-    //   forces a replacement (whenever there's already a supergraph binary of the right version
-    //   installed) and other times it just intsalls the plugin
-    // WHEN
-    //   - it's run
+async fn e2e_test_rover_install_plugins_from_latest_plugin_config_file(
+    #[case] binary_name: &str,
+    #[case] command_version_name: &str,
+    #[case] config_version_name: &str,
+) {
     let temp_dir = Utf8PathBuf::try_from(TempDir::new().unwrap().path().to_path_buf()).unwrap();
     let bin_path = temp_dir.join(".rover/bin");
     let mut cmd = Command::cargo_bin("rover").expect("Could not find necessary binary");
 
-    let contents = std::fs::read_to_string()
+    let config_file_contents = std::fs::read_to_string("latest_plugin_versions.json")
         .expect("Should have been able to read the file");
 
+    let versions: Value = serde_json::from_str(&config_file_contents)
+        .expect("failed to get json out of latest_plugin_versions.json");
+
+    let latest_version = &versions[binary_name]["versions"][config_version_name]
+        .to_string()
+        .replace("v", "=")
+        .replace("\"", "");
+
     cmd.env("APOLLO_HOME", temp_dir.clone());
-    //cmd.args(args);
+    cmd.args([
+        "install",
+        "--plugin",
+        &format!("{binary_name}@{command_version_name}"),
+    ]);
+    cmd.output().expect("Could not run command");
 
     // THEN
     //   - it successfully installs
+    let formatted_latest_version = latest_version.replace("=", "-v");
+    let downloaded_binary_name = format!("{binary_name}{formatted_latest_version}");
+
     let installed = bin_path
         .read_dir()
         .expect("unable to read contents of directory")
@@ -160,7 +178,7 @@ async fn e2e_test_rover_install_plugins_from_latest_plugin_config_file() {
             f.file_name()
                 .to_str()
                 .expect("failed to convert directroy filename to str")
-                .contains(binary_name)
+                .contains(&downloaded_binary_name)
         });
 
     assert_that!(installed).is_true();
