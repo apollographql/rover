bb7208b08b5e7d493fe803a9e38254749af2d3c1
jonathanrainer <jonathan.rainer@apollographql.com>
2024-08-06 13:13:35 +0100

    Fix up E2E tests

--- tests/e2e/install/plugin.rs
+++ tests/e2e/install/plugin.rs
@@ -1,26 +1,26 @@
 use std::process::Command;
+use std::str::from_utf8;
 
 use assert_cmd::prelude::CommandCargoExt;
 use assert_fs::TempDir;
 use camino::Utf8PathBuf;
 use regex::Regex;
-use rstest::fixture;
-use rstest::rstest;
+use rstest::{fixture, rstest};
 use serde_json::Value;
-use speculoos::{assert_that, boolean::BooleanAssertions};
+use speculoos::{assert_that, asserting, boolean::BooleanAssertions};
 use tracing_test::traced_test;
 
 #[rstest]
-#[case::installs_supergraph_at_pinned_version(Vec::from(["install", "--plugin", "supergraph@=2.8.0"]), "supergraph-v2.8.0")]
-#[case::installs_supergraph_at_latest(Vec::from(["install", "--plugin", "supergraph@latest-2"]), "supergraph-")]
-#[case::installs_router_at_pinned_version(Vec::from(["install", "--plugin", "router@=1.0.0"]), "router-v1.0.0")]
-#[case::installs_router_at_latest(Vec::from(["install", "--plugin", "router@latest"]), "router-")]
+#[case::installs_supergraph_at_pinned_version(Vec::from(["install", "--plugin", "supergraph@=2.8.0", "--client-timeout", "120"]), "supergraph-v2.8.0")]
+#[case::installs_supergraph_at_latest(Vec::from(["install", "--plugin", "supergraph@latest-2", "--client-timeout", "120"]), "supergraph-")]
+#[case::installs_router_at_pinned_version(Vec::from(["install", "--plugin", "router@=1.0.0", "--client-timeout", "120"]), "router-v1.0.0")]
+#[case::installs_router_at_latest(Vec::from(["install", "--plugin", "router@latest", "--client-timeout", "120"]), "router-")]
 #[ignore]
 #[tokio::test(flavor = "multi_thread")]
 #[traced_test]
 async fn e2e_test_rover_install_plugin(#[case] args: Vec<&str>, #[case] binary_name: &str) {
     // GIVEN
-    //   - a install command for the supergraph binary that forces replacement; sometimes this
+    //   - an install command for the supergraph binary that forces replacement; sometimes this
     //   forces a replacement (whenever there's already a supergraph binary of the right version
     //   installed) and other times it just intsalls the plugin
     // WHEN
@@ -30,6 +30,14 @@ async fn e2e_test_rover_install_plugin(#[case] args: Vec<&str>, #[case] binary_n
     let mut cmd = Command::cargo_bin("rover").expect("Could not find necessary binary");
     cmd.env("APOLLO_HOME", temp_dir.clone());
     cmd.args(args);
+    let output = cmd.output().expect("Could not run command");
+
+    asserting(&format!(
+        "Was expecting success but instead got: {}",
+        from_utf8(output.stderr.as_slice()).unwrap()
+    ))
+    .that(&output.status.success())
+    .is_true();
 
     // THEN
     //   - it successfully installs
@@ -41,7 +49,7 @@ async fn e2e_test_rover_install_plugin(#[case] args: Vec<&str>, #[case] binary_n
         .any(|f| {
             f.file_name()
                 .to_str()
-                .expect("failed to convert directroy filename to str")
+                .expect("failed to convert directory filename to str")
                 .contains(binary_name)
         });
 
@@ -57,14 +65,15 @@ fn temp_dir() -> Utf8PathBuf {
 }
 
 #[rstest]
-#[case::force_installs_supergraph(Vec::from(["install", "--force", "--plugin", "supergraph@=2.8.0", "--log", "debug"]), "supergraph-v2.8.0")]
-#[case::force_installs_router(Vec::from(["install", "--force", "--plugin", "router@=1.0.0", "--log", "debug"]), "router-v1.0.0")]
+#[case::force_installs_supergraph(Vec::from(["install", "--force", "--plugin", "supergraph@=2.8.0", "--log", "debug"]), "supergraph", "supergraph-v2.8.0")]
+#[case::force_installs_router(Vec::from(["install", "--force", "--plugin", "router@=1.0.0", "--log", "debug"]), "router",  "router-v1.0.0")]
 #[ignore]
 #[tokio::test(flavor = "multi_thread")]
 #[traced_test]
 async fn e2e_test_rover_install_plugin_with_force_opt(
     #[case] args: Vec<&str>,
-    #[case] binary_name: &str,
+    #[case] binary: &str,
+    #[case] binary_filename: &str,
     temp_dir: &Utf8PathBuf,
 ) {
     let bin_path = temp_dir.join(".rover/bin");
@@ -82,7 +91,11 @@ async fn e2e_test_rover_install_plugin_with_force_opt(
     cmd.args(args_without_force_option.clone());
     let output = cmd.output().expect("Could not run command");
     let stderr = std::str::from_utf8(&output.stderr).expect("failed to convert bytes to a str");
-    let re = Regex::new("the 'supergraph' plugin was successfully installed").unwrap();
+    let re = Regex::new(&format!(
+        "the '{}' plugin was successfully installed",
+        binary
+    ))
+    .unwrap();
     assert_that(&re.is_match(stderr)).is_true();
 
     let installed = bin_path
@@ -94,7 +107,7 @@ async fn e2e_test_rover_install_plugin_with_force_opt(
             f.file_name()
                 .to_str()
                 .expect("failed to convert directroy filename to str")
-                .contains(binary_name)
+                .contains(binary_filename)
         });
     assert_that(&installed).is_true();
 
@@ -114,8 +127,8 @@ async fn e2e_test_rover_install_plugin_with_force_opt(
         .any(|f| {
             f.file_name()
                 .to_str()
-                .expect("failed to convert directroy filename to str")
-                .contains(binary_name)
+                .expect("failed to convert directory filename to str")
+                .contains(binary_filename)
         });
     assert_that!(installed).is_true();
 
@@ -125,7 +138,11 @@ async fn e2e_test_rover_install_plugin_with_force_opt(
     cmd.args(forced_args);
     let output = cmd.output().expect("Could not run command");
     let stderr = std::str::from_utf8(&output.stderr).expect("failed to convert bytes to a str");
-    let re = Regex::new("the 'supergraph' plugin was successfully installed").unwrap();
+    let re = Regex::new(&format!(
+        "the '{}' plugin was successfully installed",
+        binary
+    ))
+    .unwrap();
     assert_that!(re.is_match(stderr)).is_true();
 }
 
