f7c99732d3daf72dbb947717e6642d3d3074ce80
Jonathan Rainer <Jonathan.rainer@apollographql.com>
2024-08-09 08:30:57 +0100

    ROVER-97 Add support for contract variants (#2036)
    Extend the GraphQL query for subgraphs to pull back a base variant as well as the graphVariant's subgraphs and then coalesce that together into a single response later on. This way we can support contract variants when used in the --graph-ref argument

--- crates/rover-client/src/operations/subgraph/fetch_all/types.rs
+++ crates/rover-client/src/operations/subgraph/fetch_all/types.rs
@@ -2,6 +2,9 @@ use apollo_federation_types::config::{SchemaSource, SubgraphConfig};
 use buildstructor::Builder;
 use derive_getters::Getters;
 
+pub(crate) use subgraph_fetch_all_query::SubgraphFetchAllQueryVariantOnGraphVariant;
+pub(crate) use subgraph_fetch_all_query::SubgraphFetchAllQueryVariantOnGraphVariantSourceVariant;
+
 use crate::shared::GraphRef;
 
 use super::runner::subgraph_fetch_all_query;
@@ -9,6 +12,7 @@ use super::runner::subgraph_fetch_all_query;
 pub(crate) type SubgraphFetchAllResponseData = subgraph_fetch_all_query::ResponseData;
 pub(crate) type SubgraphFetchAllGraphVariant =
     subgraph_fetch_all_query::SubgraphFetchAllQueryVariant;
+
 pub(crate) type QueryVariables = subgraph_fetch_all_query::Variables;
 
 #[derive(Debug, Clone, Eq, PartialEq)]
@@ -37,5 +41,34 @@ impl From<Subgraph> for SubgraphConfig {
             routing_url: value.url,
             schema: SchemaSource::Sdl { sdl: value.sdl },
         }
+    }
+}
+
+impl From<subgraph_fetch_all_query::SubgraphFetchAllQueryVariantOnGraphVariantSubgraphs>
+    for Subgraph
+{
+    fn from(
+        value: subgraph_fetch_all_query::SubgraphFetchAllQueryVariantOnGraphVariantSubgraphs,
+    ) -> Self {
+        Subgraph::builder()
+            .name(value.name)
+            .and_url(value.url)
+            .sdl(value.active_partial_schema.sdl)
+            .build()
+    }
+}
+
+impl
+    From<subgraph_fetch_all_query::SubgraphFetchAllQueryVariantOnGraphVariantSourceVariantSubgraphs>
+    for Subgraph
+{
+    fn from(
+        value: subgraph_fetch_all_query::SubgraphFetchAllQueryVariantOnGraphVariantSourceVariantSubgraphs,
+    ) -> Self {
+        Subgraph::builder()
+            .name(value.name)
+            .and_url(value.url)
+            .sdl(value.active_partial_schema.sdl)
+            .build()
     }
 }
