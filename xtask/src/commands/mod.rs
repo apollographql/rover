975d7672ce7e2bd4dd8efa415e6e83ae07659b66
jonathanrainer <jonathan.rainer@apollographql.com>
2024-09-02 13:48:33 +0100

    Clean up `xtask` and our CircleCI file
    This stops us running the flyby integration tests and
    `supergraph-demo` and removes the corresponding parts of xtask

--- xtask/src/commands/mod.rs
+++ xtask/src/commands/mod.rs
@@ -1,22 +1,16 @@
 pub(crate) use dist::Dist;
 pub(crate) use docs::Docs;
-pub(crate) use github_action::GithubActions;
-pub(crate) use integration_test::IntegrationTest;
 pub(crate) use lint::Lint;
 pub(crate) use package::Package;
 pub(crate) use prep::Prep;
 pub(crate) use security_check::SecurityCheck;
 pub(crate) use test::Test;
-pub(crate) use unit_test::UnitTest;
 
 pub(crate) mod dist;
 pub(crate) mod docs;
-pub(crate) mod github_action;
-pub(crate) mod integration_test;
 pub(crate) mod lint;
 pub(crate) mod package;
 pub(crate) mod prep;
 pub(crate) mod security_check;
 pub(crate) mod test;
-pub(crate) mod unit_test;
 pub(crate) mod version;
