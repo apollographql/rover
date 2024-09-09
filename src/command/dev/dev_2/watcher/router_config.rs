e9f28a7aad29c0420bf01546b5b21c4fd63a8931
Brian George <brian@apollographql.com>
2024-09-09 15:15:16 -0400

    Merge dotdat/rover-dev-refactor

---
+++ src/command/dev/dev_2/watcher/router_config.rs
@@ -0,0 +1,37 @@
+use camino::Utf8PathBuf;
+use futures::{stream::BoxStream, StreamExt};
+
+use crate::command::dev::dev_2::subtask::SubtaskHandleUnit;
+
+use super::file::FileWatcher;
+
+#[derive(Clone, Debug)]
+pub enum RouterConfigMessage {
+    Changed(String),
+}
+
+pub struct RouterConfigWatcher {
+    file_watcher: FileWatcher,
+}
+
+impl RouterConfigWatcher {
+    pub fn new(file_watcher: FileWatcher) -> RouterConfigWatcher {
+        RouterConfigWatcher { file_watcher }
+    }
+}
+
+impl SubtaskHandleUnit for RouterConfigWatcher {
+    type Output = RouterConfigMessage;
+
+    fn handle(
+        self,
+        sender: tokio::sync::mpsc::UnboundedSender<Self::Output>,
+    ) -> tokio::task::AbortHandle {
+        tokio::spawn(async move {
+            while let Some(contents) = self.file_watcher.clone().watch().next().await {
+                sender.send(RouterConfigMessage::Changed(contents));
+            }
+        })
+        .abort_handle()
+    }
+}
