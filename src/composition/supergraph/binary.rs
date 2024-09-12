f0b9b56d6152002b6d7eed9c088065b1f775f27d
Aaron Arinder <aaronarinder@gmail.com>
2024-09-12 16:43:44 -0400

    wip(composition): todo'd impl subtaskhandlestream

--- src/composition/supergraph/binary.rs
+++ src/composition/supergraph/binary.rs
@@ -2,7 +2,7 @@ use camino::Utf8PathBuf;
 use tap::TapFallible;
 
 use crate::{
-    composition::{events::CompositionEvent, CompositionError, CompositionSuccess},
+    composition::{CompositionError, CompositionSuccess},
     utils::effect::{exec::ExecCommand, read_file::ReadFile},
 };
 
