667976954d898c296bccdc9fb957157a1d5d47cf
Dan <danbond@protonmail.com>
2024-09-13 05:22:55 -0700

    dev leader deprecation (#2132)
    ## Description
    
    Initial PR the remove leader/follower related code so we can build upon
    implementing new `rover dev` command while keeping changes atomic.

--- src/command/dev/mod.rs
+++ src/command/dev/mod.rs
@@ -20,22 +20,13 @@ mod do_dev;
 mod introspect;
 
 #[cfg(feature = "composition-js")]
-mod protocol;
-
-#[cfg(feature = "composition-js")]
 mod router;
-
-#[cfg(feature = "composition-js")]
-mod schema;
-
-#[cfg(feature = "composition-js")]
-mod netstat;
 
 #[cfg(not(feature = "composition-js"))]
 mod no_dev;
 
 #[cfg(feature = "composition-js")]
-mod watcher;
+mod types;
 
 #[derive(Debug, Serialize, Parser)]
 pub struct Dev {
