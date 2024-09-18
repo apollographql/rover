dd94f5795aa93de50abad00212af35f189dfbd17
Brian George <brian@apollographql.com>
2024-09-18 12:54:06 -0400

    Test

--- src/utils/effect/test/mod.rs
+++ src/utils/effect/test/mod.rs
@@ -1,2 +1,3 @@
 pub const SUBGRAPH_INTROSPECTION_QUERY: &str =
     include_str!("./subgraph_introspection_query.graphql");
+pub const SUBGRAPH_FETCH_QUERY: &str = include_str!("./subgraph_fetch_query.graphql");
