12673702456f864979708f5610ffe25dbe459a50
Dylan Anthony <dylan@apollographql.com>
2024-09-23 12:00:21 -0600

    Extract the composition logic from `do_compose` into `Composer`

--- src/command/dev/compose.rs
+++ src/command/dev/compose.rs
@@ -2,95 +2,75 @@ use std::fs;
 use std::io::prelude::*;
 
 use anyhow::{Context, Error};
+use apollo_federation_types::rover::BuildResult;
 use camino::Utf8PathBuf;
+use rover_client::RoverClientError;
 use rover_std::{errln, Fs};
 
 use crate::command::dev::do_dev::log_err_and_continue;
-use crate::command::supergraph::compose::{Compose, CompositionOutput};
-use crate::federation::supergraph_config::ResolvedSupergraphConfig;
-use crate::options::PluginOpts;
-use crate::utils::client::StudioClientConfig;
+use crate::federation::Composer;
 use crate::{RoverError, RoverResult};
 
 #[derive(Debug)]
 pub(crate) struct ComposeRunner {
-    compose: Compose,
-    override_install_path: Option<Utf8PathBuf>,
-    client_config: StudioClientConfig,
+    pub(crate) composer: Composer,
     write_path: Utf8PathBuf,
-    composition_state: Option<RoverResult<CompositionOutput>>,
+    composition_state: Option<BuildResult>, // TODO: this doesn't need to be an option because we compose on startup now
 }
 
 impl ComposeRunner {
-    pub(crate) async fn new(
-        compose_opts: PluginOpts,
-        override_install_path: Option<Utf8PathBuf>,
-        client_config: StudioClientConfig,
-        write_path: Utf8PathBuf,
-    ) -> RoverResult<Self> {
-        let compose = Compose::new(compose_opts);
+    pub(crate) async fn new(composer: Composer, write_path: Utf8PathBuf) -> RoverResult<Self> {
         Ok(Self {
-            compose,
-            override_install_path,
-            client_config,
+            composer,
             write_path,
             composition_state: None,
         })
     }
 
     /// TODO: extract router-focused state handling somewhere else, so this can be re-used by lsp
-    pub async fn run(
-        &mut self,
-        supergraph_config: &ResolvedSupergraphConfig,
-    ) -> Result<Option<CompositionOutput>, String> {
-        let prev_state = self.composition_state();
-        self.composition_state = Some(
-            self.compose
-                .exec(
-                    self.override_install_path.clone(),
-                    self.client_config.clone(),
-                    supergraph_config.clone(),
-                    None,
-                )
-                .await,
-        );
-        let new_state = self.composition_state();
+    /// TODO: Why do we return successful output here?
+    pub async fn run(&mut self) -> RoverResult<()> {
+        let prev_state = self.composition_state.take();
+        let new_state = self.composer.compose(None).await?;
 
-        match (prev_state, new_state) {
+        let output = match (prev_state, &new_state) {
             // wasn't composed, now composed
-            (None, Some(Ok(new_success))) | (Some(Err(_)), Some(Ok(new_success))) => {
+            (None, Ok(new_success)) | (Some(Err(_)), Ok(new_success)) => {
                 let _ = self
                     .update_supergraph_schema(&new_success.supergraph_sdl)
                     .map_err(log_err_and_continue);
-                Ok(Some(new_success))
+                Ok(())
             }
             // had a composition error, now a new composition error
-            (Some(Err(prev_err)), Some(Err(new_err))) => {
-                if prev_err != new_err {
+            (Some(Err(prev_err)), Err(new_err)) => {
+                if prev_err != *new_err {
                     let _ = self.remove_supergraph_schema();
                 }
-                Err(new_err)
+                Err(RoverError::from(RoverClientError::BuildErrors {
+                    source: new_err.clone(),
+                    num_subgraphs: self.composer.supergraph_config.subgraphs.len(),
+                }))
             }
             // had a successful composition, now a new successful composition
-            (Some(Ok(prev_success)), Some(Ok(new_success))) => {
-                if prev_success != new_success {
+            (Some(Ok(prev_success)), Ok(new_success)) => {
+                if prev_success != *new_success {
                     let _ = self.update_supergraph_schema(&new_success.supergraph_sdl);
-                    Ok(Some(new_success))
+                    Ok(())
                 } else {
-                    Ok(None)
+                    Ok(()) // TODO: don't restart the router up a level, so this will be no-op
                 }
             }
-            // not composed (this should be unreachable in practice)
-            (_, None) => {
-                let _ = self.remove_supergraph_schema();
-                Ok(None)
-            }
             // now has an error
-            (_, Some(Err(new_err))) => {
+            (_, Err(new_err)) => {
                 let _ = self.remove_supergraph_schema();
-                Err(new_err)
+                Err(RoverError::from(RoverClientError::BuildErrors {
+                    source: new_err.clone(),
+                    num_subgraphs: self.composer.supergraph_config.subgraphs.len(),
+                }))
             }
-        }
+        };
+        self.composition_state = Some(new_state);
+        output
     }
 
     fn remove_supergraph_schema(&self) -> RoverResult<()> {
@@ -127,12 +107,5 @@ impl ComposeRunner {
             }
             Err(e) => Err(RoverError::new(Error::new(e).context(context))),
         }
-    }
-
-    pub fn composition_state(&self) -> Option<Result<CompositionOutput, String>> {
-        self.composition_state.as_ref().map(|s| match s {
-            Ok(comp) => Ok(comp.clone()),
-            Err(err) => Err(err.to_string()),
-        })
     }
 }
--- src/command/lsp/mod.rs
+++ src/command/lsp/mod.rs
@@ -1,18 +1,13 @@
-use core::panic;
-
-use apollo_federation_types::{
-    config::FederationVersion, javascript::SubgraphDefinition, rover::BuildErrors,
-};
+use anyhow::anyhow;
+use apollo_federation_types::javascript::SubgraphDefinition;
 use apollo_language_server::{ApolloLanguageServer, Config};
 use clap::Parser;
 use futures::{channel::mpsc::Receiver, StreamExt};
 use serde::Serialize;
 use tower_lsp::Server;
 
-use super::supergraph::compose::Compose;
-use crate::federation::supergraph_config::{
-    get_supergraph_config, ResolvedSubgraphConfig, ResolvedSupergraphConfig,
-};
+use crate::federation::supergraph_config::{get_supergraph_config, resolve_supergraph_config};
+use crate::federation::Composer;
 use crate::{
     options::PluginOpts,
     utils::{client::StudioClientConfig, parsers::FileDescriptorType},
@@ -75,19 +70,30 @@ async fn run_composer_in_thread(
     lsp_opts: LspOpts,
     client_config: StudioClientConfig,
     language_server: ApolloLanguageServer,
-) {
-    let composer = Compose::new(lsp_opts.plugin_opts.clone());
-    let federation_version =
-        dbg!(get_federation_version(lsp_opts.clone(), client_config.clone()).await);
-    match composer
-        .maybe_install_supergraph(None, client_config.clone(), federation_version.clone())
-        .await
-    {
-        Ok(_) => {}
-        Err(err) => {
-            panic!("Failed to install supergraph plugin: {:?}", err);
-        }
-    };
+) -> RoverResult<()> {
+    let initial_config = get_supergraph_config(
+        &None,
+        lsp_opts.supergraph_yaml.as_ref(),
+        None,
+        client_config.clone(),
+        &lsp_opts.plugin_opts.profile,
+    )
+    .await?
+    .ok_or_else(|| anyhow!("error getting supergraph config"))?;
+    let resolved_config = resolve_supergraph_config(
+        initial_config,
+        client_config.clone(),
+        &lsp_opts.plugin_opts.profile,
+    )
+    .await?;
+    let mut composer = Composer::new(
+        resolved_config,
+        None,
+        client_config,
+        lsp_opts.plugin_opts.elv2_license_accepter,
+        lsp_opts.plugin_opts.skip_update,
+    )
+    .await?;
 
     while let Some(mut definitions) = receiver.next().await {
         while let Some(next_definitions) = receiver.try_next().ok().flatten() {
@@ -96,36 +102,26 @@ async fn run_composer_in_thread(
         tracing::info!("Received message: {:?}", definitions);
         dbg!(&definitions);
 
-        let subgraphs = definitions
-            .into_iter()
-            .map(|def| (def.name, ResolvedSubgraphConfig::new(def.url, def.sdl)))
-            .collect();
-        let supergraph_config = ResolvedSupergraphConfig {
-            subgraphs,
-            federation_version: federation_version.clone(),
-        };
+        for subgraph in definitions {
+            if let Some(subgraph_config) =
+                composer.supergraph_config.subgraphs.get_mut(&subgraph.name)
+            {
+                subgraph_config.schema.sdl = subgraph.sdl;
+            }
+        }
 
-        match composer
-            .compose(None, client_config.clone(), supergraph_config, None)
-            .await
-        {
-            Ok(RoverOutput::CompositionResult(composition_output)) => {
-                dbg!(&composition_output);
+        match composer.compose(None).await {
+            Ok(Ok(output)) => {
+                dbg!(&output);
                 language_server
                     .composition_did_update(
-                        Some(composition_output.supergraph_sdl),
-                        composition_output
-                            .hints
-                            .into_iter()
-                            .map(Into::into)
-                            .collect(),
+                        Some(output.supergraph_sdl),
+                        output.hints.into_iter().map(Into::into).collect(),
                         None,
                     )
                     .await
             }
-            Err(rover_error) => {
-                dbg!(&rover_error);
-                let build_errors: BuildErrors = rover_error.into();
+            Ok(Err(build_errors)) => {
                 dbg!(&build_errors);
                 language_server
                     .composition_did_update(
@@ -135,33 +131,10 @@ async fn run_composer_in_thread(
                     )
                     .await
             }
-            _ => panic!("Expected CompositionResult"),
+            Err(err) => {
+                return Err(err);
+            }
         }
     }
-}
-
-async fn get_federation_version(
-    lsp_opts: LspOpts,
-    client_config: StudioClientConfig,
-) -> FederationVersion {
-    if let Some(supergraph_yaml) = &lsp_opts.supergraph_yaml {
-        if let Ok(Some(supergraph_config)) = get_supergraph_config(
-            &None,
-            Some(supergraph_yaml),
-            None,
-            client_config.clone(),
-            &lsp_opts.plugin_opts.profile,
-        )
-        .await
-        {
-            supergraph_config
-                .get_federation_version()
-                .unwrap_or(FederationVersion::LatestFedTwo)
-        } else {
-            tracing::warn!("Failed to resolve supergraph yaml");
-            FederationVersion::LatestFedTwo
-        }
-    } else {
-        FederationVersion::LatestFedTwo
-    }
+    Ok(())
 }
