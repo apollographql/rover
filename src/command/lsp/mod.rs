a1f86b2561b08199cbddd87bb59cddc79e16374c
Dylan Anthony <dylan@apollographql.com>
2024-09-23 17:11:38 -0600

    Convert LSP to use new `Watcher` struct

--- src/command/lsp/mod.rs
+++ src/command/lsp/mod.rs
@@ -1,13 +1,12 @@
 use anyhow::anyhow;
-use apollo_federation_types::javascript::SubgraphDefinition;
+use apollo_federation_types::config::SupergraphConfig;
 use apollo_language_server::{ApolloLanguageServer, Config};
 use clap::Parser;
-use futures::{channel::mpsc::Receiver, StreamExt};
 use serde::Serialize;
 use tower_lsp::Server;
 
-use crate::federation::supergraph_config::{get_supergraph_config, resolve_supergraph_config};
-use crate::federation::Composer;
+use crate::federation::supergraph_config::get_supergraph_config;
+use crate::federation::{Event, Watcher};
 use crate::{
     options::PluginOpts,
     utils::{client::StudioClientConfig, parsers::FileDescriptorType},
@@ -37,40 +36,12 @@ impl Lsp {
             .plugin_opts
             .prompt_for_license_accept(&client_config)?;
 
-        run_lsp(client_config, &self.opts).await;
+        run_lsp(client_config, &self.opts).await?;
         Ok(RoverOutput::EmptySuccess)
     }
 }
 
-async fn run_lsp(client_config: StudioClientConfig, lsp_opts: &LspOpts) {
-    let (service, socket, receiver) = ApolloLanguageServer::build_service(Config {
-        root_uri: "".into(),
-        enable_auto_composition: true,
-        force_federation: false,
-        disable_telemetry: false,
-    });
-
-    let language_server = service.inner().clone();
-
-    tokio::spawn(run_composer_in_thread(
-        receiver,
-        lsp_opts.clone(),
-        client_config,
-        language_server,
-    ));
-
-    let stdin = tokio::io::stdin();
-    let stdout = tokio::io::stdout();
-    let server = Server::new(stdin, stdout, socket);
-    server.serve(service).await;
-}
-
-async fn run_composer_in_thread(
-    mut receiver: Receiver<Vec<SubgraphDefinition>>,
-    lsp_opts: LspOpts,
-    client_config: StudioClientConfig,
-    language_server: ApolloLanguageServer,
-) -> RoverResult<()> {
+async fn run_lsp(client_config: StudioClientConfig, lsp_opts: &LspOpts) -> RoverResult<()> {
     let initial_config = get_supergraph_config(
         &None,
         lsp_opts.supergraph_yaml.as_ref(),
@@ -80,38 +51,63 @@ async fn run_composer_in_thread(
     )
     .await?
     .ok_or_else(|| anyhow!("error getting supergraph config"))?;
-    let resolved_config = resolve_supergraph_config(
+
+    let (service, socket, _receiver) = ApolloLanguageServer::build_service(
+        Config {
+            root_uri: "".into(),
+            enable_auto_composition: false,
+            force_federation: false,
+            disable_telemetry: false,
+        },
+        initial_config
+            .clone()
+            .into_iter()
+            .map(|(subgraph_name, subgraph_definition)| (subgraph_name, subgraph_definition.schema))
+            .collect(),
+    );
+
+    let language_server = service.inner().clone();
+
+    tokio::spawn(run_composer_in_thread(
         initial_config,
-        client_config.clone(),
-        &lsp_opts.plugin_opts.profile,
-    )
-    .await?;
-    let mut composer = Composer::new(
-        resolved_config,
+        lsp_opts.clone(),
+        client_config,
+        language_server,
+    ));
+
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+    let server = Server::new(stdin, stdout, socket);
+    server.serve(service).await;
+    Ok(())
+}
+
+async fn run_composer_in_thread(
+    initial_config: SupergraphConfig,
+    lsp_opts: LspOpts,
+    client_config: StudioClientConfig,
+    language_server: ApolloLanguageServer,
+) -> RoverResult<()> {
+    let watcher = Watcher::new(
+        initial_config,
         None,
         client_config,
         lsp_opts.plugin_opts.elv2_license_accepter,
         lsp_opts.plugin_opts.skip_update,
+        &lsp_opts.plugin_opts.profile,
+        1,
     )
     .await?;
 
-    while let Some(mut definitions) = receiver.next().await {
-        while let Some(next_definitions) = receiver.try_next().ok().flatten() {
-            definitions = next_definitions
-        }
-        tracing::info!("Received message: {:?}", definitions);
-        dbg!(&definitions);
+    let mut events = watcher.watch().await;
 
-        for subgraph in definitions {
-            if let Some(subgraph_config) =
-                composer.supergraph_config.subgraphs.get_mut(&subgraph.name)
-            {
-                subgraph_config.schema.sdl = subgraph.sdl;
+    while let Some(event) = events.recv().await {
+        match event {
+            Event::SubgraphUpdated { .. } => {
+                language_server.composition_did_start().await;
             }
-        }
-
-        match composer.compose(None).await {
-            Ok(Ok(output)) => {
+            Event::InitialComposition(output)
+            | Event::ComposedAfterSubgraphUpdated { output, .. } => {
                 dbg!(&output);
                 language_server
                     .composition_did_update(
@@ -121,7 +117,7 @@ async fn run_composer_in_thread(
                     )
                     .await
             }
-            Ok(Err(build_errors)) => {
+            Event::CompositionErrors(build_errors) => {
                 dbg!(&build_errors);
                 language_server
                     .composition_did_update(
@@ -131,7 +127,7 @@ async fn run_composer_in_thread(
                     )
                     .await
             }
-            Err(err) => {
+            Event::CompositionFailed(err) => {
                 return Err(err);
             }
         }
