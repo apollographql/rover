c657c4a7622ccbdcc8fc3090da07b1eb6960868e
Dylan Anthony <dylan@apollographql.com>
2024-10-01 10:38:16 -0600

    release: v0.27.0-preview.0

--- installers/binstall/scripts/nix/install.sh
+++ installers/binstall/scripts/nix/install.sh
@@ -20,7 +20,7 @@ BINARY_DOWNLOAD_PREFIX="https://github.com/apollographql/rover/releases/download
 # Rover version defined in root cargo.toml
 # Note: this line is built automatically
 # in build.rs. Don't touch it!
-PACKAGE_VERSION="v0.27.0-alpha.1"
+PACKAGE_VERSION="v0.27.0-preview.0"
 
 download_binary_and_run_installer() {
     downloader --check
