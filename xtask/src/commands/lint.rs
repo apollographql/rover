789deee921ef36993685b6801ca407784a18d8a8
Dan Bond <danbond@protonmail.com>
2024-10-01 14:00:47 +0100

    xtask/lint: remove lychee runner

--- xtask/src/commands/lint.rs
+++ xtask/src/commands/lint.rs
@@ -1,9 +1,6 @@
 use anyhow::Result;
 use clap::Parser;
 
-#[cfg(not(windows))]
-use crate::tools::LycheeRunner;
-
 use crate::tools::{CargoRunner, NpmRunner};
 
 #[derive(Debug, Parser)]
@@ -12,19 +9,7 @@ pub struct Lint {}
 impl Lint {
     pub async fn run(&self) -> Result<()> {
         CargoRunner::new()?.lint()?;
-        NpmRunner::new()?.lint()?;
-        lint_links().await
+        // TODO: do we actually need to do npm stuff?
+        NpmRunner::new()?.lint()
     }
-}
-
-#[cfg(not(windows))]
-async fn lint_links() -> Result<()> {
-    LycheeRunner::new()?.lint().await
-}
-
-#[cfg(windows)]
-async fn lint_links() -> Result<()> {
-    eprintln!("Skipping the lint checker.");
-
-    Ok(())
 }
--- xtask/src/tools/mod.rs
+++ xtask/src/tools/mod.rs
@@ -1,7 +1,5 @@
 pub(crate) use cargo::CargoRunner;
 pub(crate) use git::GitRunner;
-#[cfg(not(windows))]
-pub(crate) use lychee::LycheeRunner;
 pub(crate) use make::MakeRunner;
 pub(crate) use npm::NpmRunner;
 pub(crate) use runner::Runner;
@@ -19,5 +17,4 @@ mod runner;
 mod xcrun;
 
 #[cfg(not(windows))]
-mod lychee;
 mod versions;
