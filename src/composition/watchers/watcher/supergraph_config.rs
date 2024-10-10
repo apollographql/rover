d6c3b124a7ceddd077908c6ce4f546d188a693bf
Aaron Arinder <aaronarinder@gmail.com>
2024-10-10 13:44:58 -0400

    feat(SupergraphConfigDiff): support in-place changes   - adds a `changed` field to the struct   - for SDL/Subgraph changes, emits a vec of those changes   - no longer using the fed-rs get_subgraph_definitions(), which will     error if there's no in-lined SDL; we can get names elsewise

--- src/composition/watchers/watcher/supergraph_config.rs
+++ src/composition/watchers/watcher/supergraph_config.rs
@@ -61,6 +61,7 @@ impl SubtaskHandleUnit for SupergraphConfigWatcher {
 #[derive(Getters)]
 pub struct SupergraphConfigDiff {
     added: Vec<(String, SubgraphConfig)>,
+    changed: Vec<(String, SubgraphConfig)>,
     removed: Vec<String>,
 }
 
@@ -71,22 +72,20 @@ impl SupergraphConfigDiff {
         old: &SupergraphConfig,
         new: SupergraphConfig,
     ) -> Result<SupergraphConfigDiff, ConfigError> {
-        let old_subgraph_defs = old.get_subgraph_definitions().tap_err(|err| {
-            // TODO: why do we print here instead of just defering to the caller?
-            errln!(
-                "error getting subgraph definitions from the current supergraph config: {:?}",
-                err
-            )
-        })?;
+        let old_subgraph_names: HashSet<String> = old
+            .clone()
+            .into_iter()
+            .map(|(name, _config)| name)
+            .collect();
+
+        let new_subgraph_names: HashSet<String> = new
+            .clone()
+            .into_iter()
+            .map(|(name, _config)| name)
+            .collect();
 
         // Collect the subgraph definitions from the new supergraph config.
-        let new_subgraphs: BTreeMap<String, SubgraphConfig> = new.into_iter().collect();
-
-        // Collect the old and new subgraph names.
-        let old_subgraph_names: HashSet<String> =
-            HashSet::from_iter(old_subgraph_defs.iter().map(|def| def.name.to_string()));
-        let new_subgraph_names: HashSet<String> =
-            HashSet::from_iter(new_subgraphs.keys().map(|name| name.to_string()));
+        let new_subgraphs: BTreeMap<String, SubgraphConfig> = new.clone().into_iter().collect();
 
         // Compare the old and new subgraph names to find additions.
         let added_names: HashSet<String> =
@@ -102,7 +101,29 @@ impl SupergraphConfigDiff {
             .collect::<Vec<_>>();
         let removed = removed_names.into_iter().cloned().collect::<Vec<_>>();
 
-        Ok(SupergraphConfigDiff { added, removed })
+        // Find any in-place changes (eg, SDL, SchemaSource::Subgraph)
+        let mut changed = vec![];
+        for (old_name, old_config) in old.clone().into_iter() {
+            // Exclude any removed subgraphs
+            if !removed.contains(&old_name) {
+                let found_new = new
+                    .clone()
+                    .into_iter()
+                    .find(|(sub_name, _sub_config)| *sub_name == old_name);
+
+                if let Some((old_name, new_config)) = found_new {
+                    if new_config != old_config {
+                        changed.push((old_name, new_config));
+                    }
+                }
+            }
+        }
+
+        Ok(SupergraphConfigDiff {
+            added,
+            changed,
+            removed,
+        })
     }
 }
 
@@ -146,5 +167,42 @@ mod tests {
             .added()
             .contains(&("subgraph_c".to_string(), subgraph_def.clone())));
         assert!(diff.removed().contains(&"subgraph_b".to_string()));
+    }
+
+    #[test]
+    fn test_supergraph_config_diff_in_place_change() {
+        let subgraph_def = SubgraphConfig {
+            routing_url: None,
+            schema: SchemaSource::Subgraph {
+                graphref: "graph-ref".to_string(),
+                subgraph: "subgraph".to_string(),
+            },
+        };
+
+        let subgraph_def_updated = SubgraphConfig {
+            routing_url: None,
+            schema: SchemaSource::Subgraph {
+                graphref: "updated-graph-ref".to_string(),
+                subgraph: "subgraph".to_string(),
+            },
+        };
+
+        // Create an old supergraph config with subgraph definitions.
+        let old_subgraph_defs: BTreeMap<String, SubgraphConfig> =
+            BTreeMap::from([("subgraph_a".to_string(), subgraph_def.clone())]);
+        let old = SupergraphConfig::new(old_subgraph_defs, None);
+
+        // Create a new supergraph config with 1 new and 1 old subgraph definitions.
+        let new_subgraph_defs: BTreeMap<String, SubgraphConfig> =
+            BTreeMap::from([("subgraph_a".to_string(), subgraph_def_updated.clone())]);
+        let new = SupergraphConfig::new(new_subgraph_defs, None);
+
+        // Assert diff contain correct additions and removals.
+        let diff = SupergraphConfigDiff::new(&old, new).unwrap();
+
+        assert_eq!(diff.changed().len(), 1);
+        assert!(diff
+            .changed()
+            .contains(&("subgraph_a".to_string(), subgraph_def_updated.clone())));
     }
 }
