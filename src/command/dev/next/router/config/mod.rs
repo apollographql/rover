27fcfc3643e80cc52380b46854dd578f2cb6d7e5
Brian <2059306+dotdat@users.noreply.github.com>
2024-11-06 16:44:58 -0500

    Resolve router config for rover dev next (#2253)

---
+++ src/command/dev/next/router/config/mod.rs
@@ -0,0 +1,161 @@
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+use buildstructor::buildstructor;
+use camino::Utf8PathBuf;
+use http::Uri;
+use rover_std::{Fs, RoverStdError};
+use thiserror::Error;
+
+use crate::utils::effect::read_file::ReadFile;
+
+use self::{
+    parser::{ParseRouterConfigError, RouterConfigParser},
+    state::{RunRouterConfigDefault, RunRouterConfigFinal, RunRouterConfigReadConfig},
+};
+
+mod parser;
+mod state;
+
+const DEFAULT_ROUTER_IP_ADDR: IpAddr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+const DEFAULT_ROUTER_PORT: u16 = 4000;
+
+#[derive(Error, Debug)]
+pub enum ReadRouterConfigError {
+    #[error(transparent)]
+    Fs(RoverStdError),
+    #[error("Failed to read file at {}", .path)]
+    ReadFile {
+        path: Utf8PathBuf,
+        source: Box<dyn std::error::Error>,
+    },
+    #[error("{} is not valid yaml", .path)]
+    Deserialization {
+        path: Utf8PathBuf,
+        source: serde_yaml::Error,
+    },
+    #[error(transparent)]
+    Parse(#[from] ParseRouterConfigError),
+}
+
+pub struct RouterAddress {
+    host: IpAddr,
+    port: u16,
+}
+
+#[buildstructor]
+impl RouterAddress {
+    #[builder]
+    pub fn new(host: Option<IpAddr>, port: Option<u16>) -> RouterAddress {
+        let host = host.unwrap_or(DEFAULT_ROUTER_IP_ADDR);
+        let port = port.unwrap_or(DEFAULT_ROUTER_PORT);
+        RouterAddress { host, port }
+    }
+}
+
+impl Default for RouterAddress {
+    fn default() -> Self {
+        RouterAddress {
+            host: DEFAULT_ROUTER_IP_ADDR,
+            port: DEFAULT_ROUTER_PORT,
+        }
+    }
+}
+
+impl From<RouterAddress> for SocketAddr {
+    fn from(value: RouterAddress) -> Self {
+        let host = value.host;
+        let port = value.port;
+        SocketAddr::new(host, port)
+    }
+}
+
+impl From<Option<SocketAddr>> for RouterAddress {
+    fn from(value: Option<SocketAddr>) -> Self {
+        let host = value.map(|addr| addr.ip());
+        let port = value.map(|addr| addr.port());
+        RouterAddress::new(host, port)
+    }
+}
+
+impl From<SocketAddr> for RouterAddress {
+    fn from(value: SocketAddr) -> Self {
+        let host = value.ip();
+        let port = value.port();
+        RouterAddress { host, port }
+    }
+}
+
+pub struct RunRouterConfig<State> {
+    state: State,
+}
+
+impl Default for RunRouterConfig<RunRouterConfigDefault> {
+    fn default() -> Self {
+        RunRouterConfig {
+            state: RunRouterConfigDefault,
+        }
+    }
+}
+
+impl RunRouterConfig<RunRouterConfigDefault> {
+    pub fn with_address(
+        self,
+        router_address: RouterAddress,
+    ) -> RunRouterConfig<RunRouterConfigReadConfig> {
+        RunRouterConfig {
+            state: RunRouterConfigReadConfig { router_address },
+        }
+    }
+}
+
+impl RunRouterConfig<RunRouterConfigReadConfig> {
+    pub async fn with_config<ReadF: ReadFile>(
+        self,
+        read_file_impl: &ReadF,
+        path: Option<Utf8PathBuf>,
+    ) -> Result<RunRouterConfig<RunRouterConfigFinal>, ReadRouterConfigError> {
+        let state = if let Some(path) = path {
+            Fs::assert_path_exists(&path).map_err(ReadRouterConfigError::Fs)?;
+            let contents = read_file_impl.read_file(&path).await.map_err(|err| {
+                ReadRouterConfigError::ReadFile {
+                    path: path.clone(),
+                    source: Box::new(err),
+                }
+            })?;
+            let yaml = serde_yaml::from_str(&contents)
+                .map_err(|err| ReadRouterConfigError::Deserialization { path, source: err })?;
+            let router_config = RouterConfigParser::new(&yaml);
+            let address = router_config.address()?;
+            let address = address
+                .map(RouterAddress::from)
+                .unwrap_or(self.state.router_address);
+            let health_check = router_config.health_check();
+            let listen_path = router_config.listen_path()?;
+            RunRouterConfigFinal {
+                listen_path,
+                address,
+                health_check,
+            }
+        } else {
+            RunRouterConfigFinal::default()
+        };
+        Ok(RunRouterConfig { state })
+    }
+}
+
+impl RunRouterConfig<RunRouterConfigFinal> {
+    #[allow(unused)]
+    pub fn listen_path(&self) -> Option<&Uri> {
+        self.state.listen_path.as_ref()
+    }
+
+    #[allow(unused)]
+    pub fn address(&self) -> &RouterAddress {
+        &self.state.address
+    }
+
+    #[allow(unused)]
+    pub fn health_check(&self) -> bool {
+        self.state.health_check
+    }
+}
