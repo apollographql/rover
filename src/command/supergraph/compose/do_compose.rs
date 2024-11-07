28ddf4678e984ff9bea057ef6eb5b1e1f69bc939
Aaron Arinder <aaronarinder@gmail.com>
2024-11-07 15:17:16 -0500

    wip

--- src/command/supergraph/compose/do_compose.rs
+++ src/command/supergraph/compose/do_compose.rs
@@ -154,141 +154,141 @@ impl Compose {
         Ok(plugin_exe)
     }
 
-    //#[cfg(feature = "composition-rewrite")]
-    //pub async fn run(
-    //    &self,
-    //    override_install_path: Option<Utf8PathBuf>,
-    //    client_config: StudioClientConfig,
-    //    output_file: Option<Utf8PathBuf>,
-    //) -> RoverResult<RoverOutput> {
-    //    tracing::warn!("using composition-rewrite");
-    //    let mut stdin = stdin();
-    //    let write_file = FsWriteFile::default();
-    //    let read_file = FsReadFile::default();
-    //    let exec_command = TokioCommand::default();
+    #[cfg(feature = "composition-rewrite")]
+    pub async fn run(
+        &self,
+        override_install_path: Option<Utf8PathBuf>,
+        client_config: StudioClientConfig,
+        output_file: Option<Utf8PathBuf>,
+    ) -> RoverResult<RoverOutput> {
+        tracing::warn!("using composition-rewrite");
+        let mut stdin = stdin();
+        let write_file = FsWriteFile::default();
+        let read_file = FsReadFile::default();
+        let exec_command = TokioCommand::default();
 
-    //    let studio_client =
-    //        client_config.get_authenticated_client(&self.opts.plugin_opts.profile.clone())?;
+        let studio_client =
+            client_config.get_authenticated_client(&self.opts.plugin_opts.profile.clone())?;
 
-    //    let supergraph_root = self
-    //        .opts
-    //        .clone()
-    //        .supergraph_config_source()
-    //        .clone()
-    //        .supergraph_yaml
-    //        .and_then(|file| match file {
-    //            FileDescriptorType::File(file) => {
-    //                let mut current_dir =
-    //                    current_dir().expect("Unable to get current directory path");
+        let supergraph_root = self
+            .opts
+            .clone()
+            .supergraph_config_source()
+            .clone()
+            .supergraph_yaml
+            .and_then(|file| match file {
+                FileDescriptorType::File(file) => {
+                    let mut current_dir =
+                        current_dir().expect("Unable to get current directory path");
 
-    //                current_dir.push(file);
-    //                let path = Utf8PathBuf::from_path_buf(current_dir).unwrap();
-    //                let parent = path.parent().unwrap().to_path_buf();
-    //                Some(parent)
-    //            }
-    //            FileDescriptorType::Stdin => None,
-    //        });
+                    current_dir.push(file);
+                    let path = Utf8PathBuf::from_path_buf(current_dir).unwrap();
+                    let parent = path.parent().unwrap().to_path_buf();
+                    Some(parent)
+                }
+                FileDescriptorType::Stdin => None,
+            });
 
-    //    // Get a FullyResolvedSupergraphConfig from first loading in any remote subgraphs and then
-    //    // a local supergraph config (if present) and then combining them into a fully resolved
-    //    // supergraph config
-    //    let resolver = SupergraphConfigResolver::default()
-    //        .load_remote_subgraphs(
-    //            &studio_client,
-    //            self.opts.supergraph_config_source.graph_ref.as_ref(),
-    //        )
-    //        .await?
-    //        .load_from_file_descriptor(
-    //            &mut stdin,
-    //            self.opts.supergraph_config_source.supergraph_yaml.as_ref(),
-    //        )?
-    //        .fully_resolve_subgraphs(&client_config, &studio_client, supergraph_root.as_ref())
-    //        .await?;
+        // Get a FullyResolvedSupergraphConfig from first loading in any remote subgraphs and then
+        // a local supergraph config (if present) and then combining them into a fully resolved
+        // supergraph config
+        let resolver = SupergraphConfigResolver::default()
+            .load_remote_subgraphs(
+                &studio_client,
+                self.opts.supergraph_config_source.graph_ref.as_ref(),
+            )
+            .await?
+            .load_from_file_descriptor(
+                &mut stdin,
+                self.opts.supergraph_config_source.supergraph_yaml.as_ref(),
+            )?
+            .fully_resolve_subgraphs(&client_config, &studio_client, supergraph_root.as_ref())
+            .await?;
 
-    //    // We convert the FullyResolvedSupergraphConfig into a Supergraph because it makes using
-    //    // Serde easier (said differently: we're using the Federation-rs types here for
-    //    // compatability with Federation-rs tooling later on when we use their supergraph binary to
-    //    // actually run composition)
-    //    let supergraph_config: SupergraphConfig = resolver.clone().try_into()?;
+        // We convert the FullyResolvedSupergraphConfig into a Supergraph because it makes using
+        // Serde easier (said differently: we're using the Federation-rs types here for
+        // compatability with Federation-rs tooling later on when we use their supergraph binary to
+        // actually run composition)
+        let supergraph_config: SupergraphConfig = resolver.clone().try_into()?;
 
-    //    // Convert the FullyResolvedSupergraphConfig to yaml before we save it
-    //    let supergraph_config_yaml = serde_yaml::to_string(&supergraph_config)?;
+        // Convert the FullyResolvedSupergraphConfig to yaml before we save it
+        let supergraph_config_yaml = serde_yaml::to_string(&supergraph_config)?;
 
-    //    // We're going to save to a temporary place because we don't actually need the supergraph
-    //    // config to stick around; we only need it on disk to point the supergraph binary at
-    //    let supergraph_config_filepath =
-    //        Utf8PathBuf::from_path_buf(tempdir()?.path().join("supergraph.yaml"))
-    //            .expect("Unable to parse path");
+        // We're going to save to a temporary place because we don't actually need the supergraph
+        // config to stick around; we only need it on disk to point the supergraph binary at
+        let supergraph_config_filepath =
+            Utf8PathBuf::from_path_buf(tempdir()?.path().join("supergraph.yaml"))
+                .expect("Unable to parse path");
 
-    //    // Write the supergraph config to disk
-    //    let _ = write_file
-    //        .write_file(
-    //            &supergraph_config_filepath,
-    //            supergraph_config_yaml.as_bytes(),
-    //        )
-    //        .await?;
+        // Write the supergraph config to disk
+        let _ = write_file
+            .write_file(
+                &supergraph_config_filepath,
+                supergraph_config_yaml.as_bytes(),
+            )
+            .await?;
 
-    //    // Use the CLI option for federation over the one we can read off of the supergraph config
-    //    // (but default to the one we can read off the supergraph config)
-    //    let fed_version = if let Some(fed_version) = self.opts.federation_version.as_ref() {
-    //        fed_version
-    //    } else {
-    //        resolver.federation_version()
-    //    };
+        // Use the CLI option for federation over the one we can read off of the supergraph config
+        // (but default to the one we can read off the supergraph config)
+        let fed_version = if let Some(fed_version) = self.opts.federation_version.as_ref() {
+            fed_version
+        } else {
+            resolver.federation_version()
+        };
 
-    //    // We care about the exact version of the federation version because certain options aren't
-    //    // available before 2.9.0 and we gate on that version below
-    //    let exact_version = fed_version
-    //        .get_exact()
-    //        // This should be impossible to get to because we convert to a FederationVersion a few
-    //        // lines above and so _should_ have an exact version
-    //        .ok_or(RoverError::new(anyhow!(
-    //            "failed to get exact Federation version"
-    //        )))?;
+        // We care about the exact version of the federation version because certain options aren't
+        // available before 2.9.0 and we gate on that version below
+        let exact_version = fed_version
+            .get_exact()
+            // This should be impossible to get to because we convert to a FederationVersion a few
+            // lines above and so _should_ have an exact version
+            .ok_or(RoverError::new(anyhow!(
+                "failed to get exact Federation version"
+            )))?;
 
-    //    // Making the output file mutable allows us to change it if we're using a version of the
-    //    // supergraph binary that can't write to file (ie, anything pre-2.9.0)
-    //    let mut output_file = output_file;
+        // Making the output file mutable allows us to change it if we're using a version of the
+        // supergraph binary that can't write to file (ie, anything pre-2.9.0)
+        let mut output_file = output_file;
 
-    //    // When the `--output` flag is used, we need a supergraph binary version that is at least
-    //    // v2.9.0. We ignore that flag for composition when we have anything less than that
-    //    if output_file.is_some()
-    //        && (exact_version.major < 2 || (exact_version.major == 2 && exact_version.minor < 9))
-    //    {
-    //        warnln!("ignoring `--output` because it is not supported in this version of the dependent binary, `supergraph`: {}. Upgrade to Federation 2.9.0 or greater to install a version of the binary that supports it.", fed_version);
-    //        output_file = None;
-    //    }
+        // When the `--output` flag is used, we need a supergraph binary version that is at least
+        // v2.9.0. We ignore that flag for composition when we have anything less than that
+        if output_file.is_some()
+            && (exact_version.major < 2 || (exact_version.major == 2 && exact_version.minor < 9))
+        {
+            warnln!("ignoring `--output` because it is not supported in this version of the dependent binary, `supergraph`: {}. Upgrade to Federation 2.9.0 or greater to install a version of the binary that supports it.", fed_version);
+            output_file = None;
+        }
 
-    //    // Build the supergraph binary, paying special attention to the CLI options
-    //    let supergraph_binary = InstallSupergraph::builder()
-    //        .federation_version(fed_version.clone())
-    //        .elv2_license_accepter(self.opts.plugin_opts.elv2_license_accepter)
-    //        .studio_client_config(client_config.clone())
-    //        .and_override_install_path(override_install_path)
-    //        .skip_update(self.opts.plugin_opts.skip_update)
-    //        .build()
-    //        .install()
-    //        .await?;
+        // Build the supergraph binary, paying special attention to the CLI options
+        let supergraph_binary = InstallSupergraph::builder()
+            .federation_version(fed_version.clone())
+            .elv2_license_accepter(self.opts.plugin_opts.elv2_license_accepter)
+            .studio_client_config(client_config.clone())
+            .and_override_install_path(override_install_path)
+            .skip_update(self.opts.plugin_opts.skip_update)
+            .build()
+            .install()
+            .await?;
 
-    //    // Federation versions and supergraph versions are synonymous, but have different types
-    //    // representing them depending on their use (eg, we only ever have an exact
-    //    // SupergraphVersion because we can only ever use an exact version of the binary, where the
-    //    // FederationVersion can just point to latest to get the latest version--these are similar,
-    //    // but represent different ways of using the underlying version)
-    //    let supergraph_version: SupergraphVersion = fed_version.clone().try_into()?;
+        // Federation versions and supergraph versions are synonymous, but have different types
+        // representing them depending on their use (eg, we only ever have an exact
+        // SupergraphVersion because we can only ever use an exact version of the binary, where the
+        // FederationVersion can just point to latest to get the latest version--these are similar,
+        // but represent different ways of using the underlying version)
+        let supergraph_version: SupergraphVersion = fed_version.clone().try_into()?;
 
-    //    let supergraph_binary = SupergraphBinary::builder()
-    //        .exe(supergraph_binary)
-    //        .output_target(output_file)
-    //        .version(supergraph_version)
-    //        .build();
+        let supergraph_binary = SupergraphBinary::builder()
+            .exe(supergraph_binary)
+            .output_target(output_file)
+            .version(supergraph_version)
+            .build();
 
-    //    let result = supergraph_binary
-    //        .compose(&exec_command, &read_file, supergraph_config_filepath)
-    //        .await?;
+        let result = supergraph_binary
+            .compose(&exec_command, &read_file, supergraph_config_filepath)
+            .await?;
 
-    //    Ok(RoverOutput::CompositionResult(result.into()))
-    //}
+        Ok(RoverOutput::CompositionResult(result.into()))
+    }
 
     #[cfg(not(feature = "composition-rewrite"))]
     pub async fn run(
