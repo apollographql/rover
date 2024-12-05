ade986114bdc47b18a4fdea732b4465f98c1ca6b
Brian George <brian@apollographql.com>
2024-12-05 18:09:22 -0500

    Extract federation version resolution

--- src/composition/supergraph/config/resolver/state.rs
+++ src/composition/supergraph/config/resolver/state.rs
@@ -1,18 +1,22 @@
 use std::collections::BTreeMap;
 
-use apollo_federation_types::config::{FederationVersion, SubgraphConfig};
+use apollo_federation_types::config::SubgraphConfig;
 use camino::Utf8PathBuf;
+
+use crate::composition::supergraph::config::federation::{
+    FederationVersionResolverFromSubgraphs, FederationVersionResolverFromSupergraphConfig,
+};
 
 /// In this stage, we await the caller to optionally load subgraphs from the Studio API using
 /// the contents of the `--graph-ref` flag
 pub struct LoadRemoteSubgraphs {
-    pub federation_version: Option<FederationVersion>,
+    pub federation_version_resolver: FederationVersionResolverFromSupergraphConfig,
 }
 
 /// In this stage, we await the caller to optionally load subgraphs and a specified federation
 /// version from a local supergraph config file
 pub struct LoadSupergraphConfig {
-    pub federation_version: Option<FederationVersion>,
+    pub federation_version_resolver: FederationVersionResolverFromSupergraphConfig,
     pub subgraphs: BTreeMap<String, SubgraphConfig>,
 }
 
@@ -20,6 +24,6 @@ pub struct LoadSupergraphConfig {
 /// and exist) or fully: rendering the subgraph source down to an SDL
 pub struct ResolveSubgraphs {
     pub origin_path: Option<Utf8PathBuf>,
-    pub federation_version: Option<FederationVersion>,
+    pub federation_version_resolver: FederationVersionResolverFromSubgraphs,
     pub subgraphs: BTreeMap<String, SubgraphConfig>,
 }
