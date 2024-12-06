7641ed1f729b612b0735bade943faeabeba045c4
Aaron Arinder <aaronarinder@gmail.com>
2024-12-06 15:36:14 -0500

    wip: rough idea in place

--- src/command/dev/next/mod.rs
+++ src/command/dev/next/mod.rs
@@ -1,23 +1,15 @@
 #![warn(missing_docs)]
 
-use std::time::Duration;
-
 use anyhow::anyhow;
 use apollo_federation_types::config::RouterVersion;
 use camino::Utf8PathBuf;
-use chrono::Duration;
-use futures::task::Spawn;
 use houston::{Config, Profile};
-use http::StatusCode;
-use reqwest::Request;
 use router::{
     install::InstallRouter,
     run::RunRouter,
     watchers::{file::FileWatcher, router_config::RouterConfigWatcher},
 };
-use rover_client::operations::config::who_am_i::{self, WhoAmI};
-use tokio::time::sleep;
-use tower::ServiceBuilder;
+use rover_client::operations::config::who_am_i::WhoAmI;
 
 use crate::{
     command::Dev,
@@ -79,7 +71,7 @@ impl Dev {
         let graph_ref = self.opts.supergraph_opts.graph_ref.clone();
         let composition_output = tmp_config_dir_path.join("supergraph.graphql");
 
-        let one_off_composition = OneShotComposition::builder()
+        let one_shot_composition = OneShotComposition::builder()
             .client_config(client_config.clone())
             .profile(profile.clone())
             .elv2_license_accepter(elv2_license_accepter)
@@ -91,30 +83,13 @@ impl Dev {
             .and_override_install_path(override_install_path.clone())
             .build();
 
-        // FIXME: send this off to the router binary
-        let _composition_output = one_off_composition.compose().await?;
+        // The router binary will know where to find the composition result; we compose initially
+        // for the router to have a properly composed schema when starting
+        one_shot_composition.compose().await?;
 
-        let run_router = RunRouter::default();
         // TODO: figure out how to actually get this; maybe based on fed version? didn't see a cli
         // opt
         let router_version = RouterVersion::Latest;
-        let run_router = run_router
-            .install::<InstallRouter>(
-                router_version,
-                client_config.clone(),
-                override_install_path,
-                elv2_license_accepter,
-                skip_update,
-            )
-            .await?;
-
-        let service = client_config
-            .get_authenticated_client(&profile)?
-            .service()?;
-
-        let run_router = run_router
-            .load_config(&read_file_impl, router_address, router_config_path)
-            .await?;
 
         // TODO: better; weird to call config, weird to get credential this way
         // FIXME: unwraps
@@ -130,44 +105,34 @@ impl Dev {
         //  3. consider specifying the generic argument: `::<&_>` [E0283]
             Profile::get_credential(&profile.profile_name, &Config::new(None, None).unwrap())
                 .unwrap();
-
+        let service = client_config
+            .get_authenticated_client(&profile)?
+            .service()?;
         let mut service = WhoAmI::new(service);
 
-        let run_router = run_router
+        RunRouter::default()
+            .install::<InstallRouter>(
+                router_version,
+                client_config.clone(),
+                override_install_path,
+                elv2_license_accepter,
+                skip_update,
+            )
+            .await?
+            .load_config(&read_file_impl, router_address, router_config_path)
+            .await?
             // TODO: figure out if I can just pass None instead for the credential and let the
             // internal workings of it sort it out
             .load_remote_config(service, graph_ref, Some(credential))
-            .await;
-
-        // TODO: figure out if all the right files will be in the tmp_config_dir_path;
-        // watch_for_changes() wants a config.yaml in there, but I'm not sure it exists yet unless
-        // written out by some internal logic
-        run_router.watch_for_changes(write_file_impl, TokioSpawn::default(), &tmp_config_dir_path);
-
-        // TODO: this should actually come from the config if we have it; eg, the difference
-        // between /health, the default, and a custom /healthz
-        // see: https://www.apollographql.com/docs/graphos/routing/self-hosted/health-checks
-        let mut healthcheck_endpoint = router_address.host().to_string();
-        healthcheck_endpoint.push_str(":8088/health");
-
-        // FIXME: unwrap
-        let healthcheck_client = client_config.get_reqwest_client().unwrap();
-
-        let healthcheck_request = healthcheck_client
-            .get(healthcheck_endpoint)
-            .build()
-            .unwrap();
-
-        // Wait for the router to become healthy before continuing by checking its health endpoint
-        while !healthcheck_client
-            .execute(healthcheck_request)
             .await
-            .unwrap()
-            .status()
-            .is_success()
-        {
-            sleep(Duration::from_millis(100)).await;
-        }
+            .run(
+                TokioSpawn::default(),
+                &tmp_config_dir_path,
+                client_config,
+                &router_address,
+            )
+            .await?
+            .watch_for_changes(write_file_impl);
 
         // TODO: more stuff with dev, the router is alive
 
