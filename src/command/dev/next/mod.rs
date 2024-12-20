fcf638e537cd115578b27dc352bb53a5a4480e02
Dan Bond <danbond@protonmail.com>
2024-12-20 11:41:04 +0000

    print new line

--- src/command/dev/next/mod.rs
+++ src/command/dev/next/mod.rs
@@ -12,13 +12,7 @@ use rover_client::operations::config::who_am_i::WhoAmI;
 
 use crate::{
     command::Dev,
-    composition::{
-        pipeline::CompositionPipeline,
-        supergraph::config::resolver::{
-            fetch_remote_subgraph::MakeFetchRemoteSubgraph,
-            fetch_remote_subgraphs::MakeFetchRemoteSubgraphs,
-        },
-    },
+    composition::pipeline::CompositionPipeline,
     utils::{
         client::StudioClientConfig,
         effect::{
@@ -46,7 +40,6 @@ impl Dev {
         let read_file_impl = FsReadFile::default();
         let write_file_impl = FsWriteFile::default();
         let exec_command_impl = TokioCommand::default();
-
         let router_address = RouterAddress::new(
             self.opts.supergraph_opts.supergraph_address,
             self.opts.supergraph_opts.supergraph_port,
@@ -70,26 +63,17 @@ impl Dev {
         let service = client_config.get_authenticated_client(profile)?.service()?;
         let service = WhoAmI::new(service);
 
-        let make_fetch_remote_subgraphs = MakeFetchRemoteSubgraphs::builder()
-            .studio_client_config(client_config.clone())
-            .profile(profile.clone())
-            .build();
-        let make_fetch_remote_subgraph = MakeFetchRemoteSubgraph::builder()
-            .studio_client_config(client_config.clone())
-            .profile(profile.clone())
-            .build();
-
         let composition_pipeline = CompositionPipeline::default()
             .init(
                 &mut stdin(),
-                make_fetch_remote_subgraphs,
+                &client_config.get_authenticated_client(profile)?,
                 supergraph_config_path.clone(),
                 graph_ref.clone(),
             )
             .await?
             .resolve_federation_version(
                 &client_config,
-                make_fetch_remote_subgraph,
+                &client_config.get_authenticated_client(profile)?,
                 self.opts.supergraph_opts.federation_version.clone(),
             )
             .await?
@@ -153,7 +137,7 @@ impl Dev {
         loop {
             tokio::select! {
                 _ = tokio::signal::ctrl_c() => {
-                    eprintln!("received shutdown signal, stopping `rover dev` processes...");
+                    eprintln!("\nreceived shutdown signal, stopping `rover dev` processes...");
                     run_router.shutdown();
                     break
                 },
