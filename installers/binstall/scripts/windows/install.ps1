07c142a710b94e8879f2779f5430f2de0c4ebd41
Jonathan Rainer <Jonathan.rainer@apollographql.com>
2025-02-04 10:56:54 -0500

    release: v0.27.0-rc.4 (#2385)
    New release candidate with bugfixes, mostly around `rover dev` and
    routing config so testing for this RC should remain as it has been
    before but this should be more stable.

--- installers/binstall/scripts/windows/install.ps1
+++ installers/binstall/scripts/windows/install.ps1
@@ -14,7 +14,7 @@
 # version found in Rover's Cargo.toml
 # Note: this line is built automatically
 # in build.rs. Don't touch it!
-$package_version = 'v0.27.0-rc.3'
+$package_version = 'v0.27.0-rc.4'
 
 function Install-Binary($rover_install_args) {
   $old_erroractionpreference = $ErrorActionPreference
