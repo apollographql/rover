ec090f9c018c5468da6e0fa492993e8e2da1b31e
jonathanrainer <jonathan.rainer@apollographql.com>
2025-02-26 07:02:49 -0700

    ROVER-332 Ensure we deal with no_dev too

--- src/command/dev/no_dev.rs
+++ src/command/dev/no_dev.rs
@@ -1,15 +1,17 @@
 use anyhow::anyhow;
 use camino::Utf8PathBuf;
+use timber::Level;
 
-use crate::{
-    command::dev::Dev, utils::client::StudioClientConfig, RoverError, RoverOutput, RoverResult,
-};
+use crate::command::dev::Dev;
+use crate::utils::client::StudioClientConfig;
+use crate::{RoverError, RoverOutput, RoverResult};
 
 impl Dev {
     pub async fn run(
         &self,
         _override_install_path: Option<Utf8PathBuf>,
         _client_config: StudioClientConfig,
+        _log_level: Option<Level>,
     ) -> RoverResult<RoverOutput> {
         Err(RoverError::new(anyhow!(
             "rover dev is not supported on this platform"
