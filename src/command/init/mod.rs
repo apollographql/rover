29adbcf42b83198ec3b2cad7b45fa274833ad29d
Taylor Jones <taylor@apollographql.com>
2025-03-11 17:06:03 -0500

    cleaning up command args

--- src/command/init/mod.rs
+++ src/command/init/mod.rs
@@ -4,11 +4,7 @@ use serde::Serialize;
 use crate::{RoverOutput, RoverResult};
 
 #[derive(Debug, Serialize, Parser)]
-pub struct Init {
-    /// Overwrite any existing binary without prompting for confirmation.
-    #[arg(long = "force", short = 'f')]
-    pub(crate) force: bool,
-}
+pub struct Init {}
 
 impl Init {
     pub async fn run(&self) -> RoverResult<RoverOutput> {
