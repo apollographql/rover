29eb61cd2933a01668d84f086f3174212dc87384
Taylor Jones <taylor@apollographql.com>
2025-03-20 12:35:44 -0500

    fixing linting errors

--- tests/e2e/init/mod.rs
+++ tests/e2e/init/mod.rs
@@ -5,8 +5,5 @@ use rstest::rstest;
 #[ignore]
 fn e2e_test_rover_init_help() {
     let mut cmd = Command::cargo_bin("rover").unwrap();
-    cmd.arg("init")
-        .arg("--help")
-        .assert()
-        .success();
+    cmd.arg("init").arg("--help").assert().success();
 }
