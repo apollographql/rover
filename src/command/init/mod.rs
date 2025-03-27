57aecd030eec017211910dca1b70b5da55b5b659
Taylor Jones <45475656+tayrrible@users.noreply.github.com>
2025-03-27 16:22:54 -0500

    Adding prompt for project use case to init command (#2462)
    <!--
    First, 🌠 thank you 🌠 for taking the time to consider a contribution to
    Apollo!
    
    Here are some important details to follow:
    
    * ⏰ Your time is important
    To save your precious time, if the contribution you are making will
    take more than an hour, please make sure it has been discussed in an
            issue first. This is especially true for feature requests!
    
    * 💡 Features
    Feature requests can be created and discussed within a GitHub Issue.
    Be sure to search for existing feature requests (and related issues!)
    prior to opening a new request. If an existing issue covers the need,
    please upvote that issue by using the 👍 emote, rather than opening a
            new issue.
    
    * 🕷 Bug fixes
    These can be created and discussed in this repository. When fixing a
    bug,
    please _try_ to add a test which verifies the fix. If you cannot, you
    should
    still submit the PR but we may still ask you (and help you!) to create a
    test.
    
    * 📖 Contribution guidelines
    Follow https://github.com/apollographql/rover/blob/HEAD/CONTRIBUTING.md
    when submitting a pull request. Make sure existing tests still pass, and
    add
            tests for all new behavior.
    
    * ✏️ Explain your pull request
    Describe the big picture of your changes here to communicate to what
            your pull request is meant to accomplish. Provide 🔗 links 🔗 to
            associated issues!
    
    We hope you will find this to be a positive experience! Open source
    contribution can be intimidating and we hope to alleviate that pain as
    much
    as possible. Without following these guidelines, you may be missing
    context
    that can help you succeed with your contribution, which is why we
    encourage
    discussion first. Ultimately, there is no guarantee that we will be able
    to
    merge your pull-request, but by following these guidelines we can try to
    avoid disappointment.
    
    -->
    Adding a select prompt to the `init` command that allows the user to
    select between connecting a REST API or starting with recommended
    GraphQL libraries.
    
    Currently, selection is a no-op

--- src/command/init/mod.rs
+++ src/command/init/mod.rs
@@ -1,14 +1,23 @@
+use crate::options::{ProjectUseCase, ProjectUseCaseOpt};
+use crate::{RoverOutput, RoverResult};
 use clap::Parser;
 use serde::Serialize;
 
-use crate::{RoverOutput, RoverResult};
-
 #[derive(Debug, Serialize, Parser)]
-pub struct Init {}
+pub struct Init {
+    #[clap(flatten)]
+    use_case_options: ProjectUseCaseOpt,
+}
 
 impl Init {
     pub async fn run(&self) -> RoverResult<RoverOutput> {
         println!("\nWelcome! This command helps you initialize a new GraphQL API project using Apollo Federation with Apollo Router.\n");
+
+        let use_case = self.use_case_options.get_or_prompt_use_case()?;
+        match use_case {
+            ProjectUseCase::Connectors => println!("\nComing soon!\n"),
+            ProjectUseCase::GraphQLTemplate => println!("\nComing soon!\n"),
+        }
 
         Ok(RoverOutput::EmptySuccess)
     }
---
+++ src/options/project_use_case.rs
@@ -0,0 +1,104 @@
+use crate::{RoverError, RoverResult};
+use anyhow::anyhow;
+use clap::{Parser, ValueEnum};
+use console::Term;
+use dialoguer::Select;
+use serde::{Deserialize, Serialize};
+use std::fmt::{self, Display};
+
+#[derive(Debug, Clone, Serialize, Deserialize, Parser)]
+pub struct ProjectUseCaseOpt {
+    #[arg(long = "project-use-case", value_enum)]
+    pub project_use_case: Option<ProjectUseCase>,
+}
+
+impl ProjectUseCaseOpt {
+    pub fn get_or_prompt_use_case(&self) -> RoverResult<ProjectUseCase> {
+        if let Some(project_use_case) = &self.project_use_case {
+            Ok(project_use_case.clone())
+        } else {
+            let use_cases = <ProjectUseCase as ValueEnum>::value_variants();
+
+            let selection = Select::new()
+                .with_prompt("? Select use case")
+                .items(use_cases)
+                .default(0)
+                .interact_on_opt(&Term::stderr())?;
+
+            self.handle_use_case_selection(use_cases, selection)
+        }
+    }
+
+    pub fn handle_use_case_selection(
+        &self,
+        use_cases: &[ProjectUseCase],
+        selection: Option<usize>,
+    ) -> RoverResult<ProjectUseCase> {
+        match selection {
+            Some(index) => Ok(use_cases[index].clone()),
+            None => Err(RoverError::new(anyhow!("No use case selected"))),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize, clap::ValueEnum)]
+pub enum ProjectUseCase {
+    Connectors,
+    GraphQLTemplate,
+}
+
+impl Display for ProjectUseCase {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use ProjectUseCase::*;
+        let readable = match self {
+            Connectors => "Connect one or more REST APIs",
+            GraphQLTemplate => "Start a GraphQL API with recommended libraries",
+        };
+        write!(f, "{}", readable)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_use_case_with_some_project_use_case() {
+        let instance = ProjectUseCaseOpt {
+            project_use_case: Some(ProjectUseCase::Connectors),
+        };
+
+        let result = instance.get_or_prompt_use_case();
+
+        assert!(result.is_ok());
+        let use_case = result.unwrap();
+        assert_eq!(use_case, ProjectUseCase::Connectors);
+    }
+
+    #[test]
+    fn test_handle_use_case_selection_returns_use_case_with_some_selection() {
+        let instance = ProjectUseCaseOpt {
+            project_use_case: None,
+        };
+
+        let use_cases = <ProjectUseCase as ValueEnum>::value_variants();
+        let selection: usize = 0;
+        let result = instance.handle_use_case_selection(use_cases, Some(selection));
+
+        assert!(result.is_ok());
+        let use_case = result.unwrap();
+        assert_eq!(use_case, use_cases[selection].clone());
+    }
+
+    #[test]
+    fn test_handle_use_case_selection_returns_error_with_none_selection() {
+        let instance = ProjectUseCaseOpt {
+            project_use_case: None,
+        };
+
+        let use_cases = <ProjectUseCase as ValueEnum>::value_variants();
+        let result = instance.handle_use_case_selection(use_cases, None);
+
+        assert!(result.is_err());
+    }
+}
