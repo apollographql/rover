fe8c67d488f96d036dcc6b37945006b0267e17a1
Camille Lawrence <camille.lawrence@apollographql.com>
2025-04-12 23:12:50 -0400

    feat(init): Include authentication state for init flow

--- src/options/mod.rs
+++ src/options/mod.rs
@@ -20,7 +20,6 @@ mod project_organization;
 mod project_type;
 #[cfg(feature = "init")]
 mod project_use_case;
-
 mod schema;
 mod subgraph;
 mod template;
@@ -36,18 +35,17 @@ pub(crate) use output::*;
 pub(crate) use persisted_queries::*;
 pub(crate) use profile::*;
 #[cfg(feature = "init")]
-pub use project_authentication::*;
+pub(crate) use project_authentication::*;
 #[cfg(feature = "init")]
-pub use project_graphid::*;
+pub(crate) use project_graphid::*;
 #[cfg(feature = "init")]
-pub use project_name::*;
+pub(crate) use project_name::*;
 #[cfg(feature = "init")]
-pub use project_organization::*;
+pub(crate) use project_organization::*;
 #[cfg(feature = "init")]
 pub(crate) use project_type::*;
 #[cfg(feature = "init")]
 pub(crate) use project_use_case::*;
-
 pub(crate) use schema::*;
 pub(crate) use subgraph::*;
 pub(crate) use template::*;
