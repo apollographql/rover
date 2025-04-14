47b4df900d8eff9be2efb0bf00d7f2f38d0860c2
Daniel Sanchez <daniel.sanchez@apollographql.com>
2025-04-14 12:17:46 -0400

    Adding the ability to find graphql schemas in directory

--- src/command/init/transitions.rs
+++ src/command/init/transitions.rs
@@ -8,7 +8,7 @@ use rover_http::ReqwestService;
 use crate::command::init::config::ProjectConfig;
 use crate::command::init::helpers::*;
 use crate::command::init::states::*;
-use crate::command::init::template_operations::TemplateOperations;
+use crate::command::init::template_operations::{SupergraphBuilder, TemplateOperations};
 use crate::options::GraphIdOpt;
 use crate::options::ProjectNameOpt;
 use crate::options::ProjectUseCase;
@@ -231,17 +231,7 @@ impl CreationConfirmed {
         // (confirmation was done in the previous state)
         self.template.write_template(&self.output_path)?;
 
-        let subgraph = match self.config.use_case {
-            ProjectUseCase::Connectors => TemplateOperations::create_subgraph_config(
-                String::from("http://ignore"),
-                String::from("products.graphql"),
-                String::from("schema"),
-            ),
-            ProjectUseCase::GraphQLTemplate => {
-                Err(anyhow!("GraphQL Template is coming soon!").into())
-            }
-        };
-        TemplateOperations::generate_supergraph(self.output_path, subgraph.unwrap())?;
+        SupergraphBuilder::new(self.output_path, 5).build_and_write()?;
 
         let artifacts = self.template.list_files()?;
 
@@ -255,8 +245,6 @@ impl CreationConfirmed {
                 .into())
             }
         };
-
-        // write supergraph.yaml
 
         Ok(ProjectCreated {
             config: self.config,
