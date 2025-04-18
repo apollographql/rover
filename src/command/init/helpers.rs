70ad9e53829658a36ebea08f41e18b8bce3f0641
Taylor Jones <taylor@apollographql.com>
2025-04-18 15:01:40 -0500

    linting

--- src/command/init/helpers.rs
+++ src/command/init/helpers.rs
@@ -35,8 +35,16 @@ pub fn display_project_created_message(
         "{}",
         Style::Heading.paint("GraphOS credentials for your graph")
     );
-    successln!("{}={} (Formatted graph-id@variant, references a graph in the Apollo GraphOS platform)", Style::Command.paint("APOLLO_GRAPH_REF"), graph_ref);
-    successln!("{}={} (This is your graph’s API key)", Style::Command.paint("APOLLO_KEY"), api_key);
+    successln!(
+        "{}={} (Formatted graph-id@variant, references a graph in the Apollo GraphOS platform)",
+        Style::Command.paint("APOLLO_GRAPH_REF"),
+        graph_ref
+    );
+    successln!(
+        "{}={} (This is your graph’s API key)",
+        Style::Command.paint("APOLLO_KEY"),
+        api_key
+    );
     println!();
     println!("{}", Style::WarningHeading.paint("️▲ Before you proceed:"));
     println!("- Store your graph API key securely, you won’t be able to access it again!");
@@ -46,7 +54,10 @@ pub fn display_project_created_message(
     println!();
     println!(
         "{}",
-        Style::Command.paint(format!("$ APOLLO_KEY={} rover dev --graph-ref {} --supergraph-config supergraph.yaml", api_key, graph_ref))
+        Style::Command.paint(format!(
+            "$ APOLLO_KEY={} rover dev --graph-ref {} --supergraph-config supergraph.yaml",
+            api_key, graph_ref
+        ))
     );
     println!();
     println!("For more information, check out `getting-started.md`.");
