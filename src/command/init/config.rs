9f17d1d94e7e7b8aed271a42b2837bb2490a9303
Camille Lawrence <camille.lawrence@apollographql.com>
2025-04-24 13:52:02 -0400

    fix import issues

--- src/command/init/config.rs
+++ src/command/init/config.rs
@@ -1,7 +1,11 @@
+#[cfg(feature = "composition-js")]
 use crate::command::init::graph_id::validation::GraphId;
+#[cfg(feature = "composition-js")]
 use crate::command::init::options::{OrganizationId, ProjectName, ProjectType, ProjectUseCase};
+#[cfg(feature = "composition-js")]
 use serde::{Deserialize, Serialize};
 
+#[cfg(feature = "composition-js")]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ProjectConfig {
     pub project_type: ProjectType,
