041413aa95a25d5e75667673fd23276095b55f2d
Dylan Anthony <dylan@apollographql.com>
2025-08-19 17:43:24 -0600

    Add `rover connector` namespace (#2667)
    Some baseline setup for new Connectors commands coming soon. Just giving
    graph-dev a place to iterate.

--- src/cli.rs
+++ src/cli.rs
@@ -188,6 +188,8 @@ impl Rover {
             Command::Init(command) => command.run(self.get_client_config()?).await,
             Command::Cloud(command) => command.run(self.get_client_config()?).await,
             Command::Config(command) => command.run(self.get_client_config()?).await,
+            #[cfg(feature = "composition-js")]
+            Command::Connector(command) => command.run().await,
             Command::Contract(command) => command.run(self.get_client_config()?).await,
             Command::Dev(command) => {
                 command
@@ -369,6 +371,10 @@ pub enum Command {
 
     /// Cloud configuration commands
     Cloud(command::Cloud),
+
+    #[cfg(feature = "composition-js")]
+    #[clap(hide = true)]
+    Connector(command::Connector),
 
     /// Configuration profile commands
     Config(command::Config),
