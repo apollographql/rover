d34809eda91a87270e2a656a6266ab7ad45a2379
Julia Naomi <jnboeira@outlook.com>
2025-10-31 14:04:26 -0400

    windows

--- tests/snap_connectors/mod.rs
+++ tests/snap_connectors/mod.rs
@@ -1,6 +1,4 @@
 #[test]
 fn cli_tests() {
-    trycmd::TestCases::new()
-        .case("tests/snap_connectors/cmd/*.toml")
-        .case("tests/snap_connectors/cmd/*.md");
+    trycmd::TestCases::new().case("tests/snap_connectors/cmd/*.md");
 }
