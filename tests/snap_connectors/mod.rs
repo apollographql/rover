98b8fe148253781c4de460999126453409615b04
Julia Naomi <jnboeira@outlook.com>
2025-10-31 15:09:31 -0400

    test

--- tests/snap_connectors/mod.rs
+++ tests/snap_connectors/mod.rs
@@ -1,4 +1,6 @@
 #[test]
 fn cli_tests() {
-    trycmd::TestCases::new().case("tests/snap_connectors/cmd/*.md");
+    trycmd::TestCases::new()
+        .case("tests/snap_connectors/cmd/*.md")
+        .insert_var("[REPLACEMENT]", "runtime-value").unwrap();
 }
