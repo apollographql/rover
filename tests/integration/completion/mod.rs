70780aa37585de3a097710d9fdbb293452d6720d
Shane Myrick <shane@apollographql.com>
2025-11-11 15:46:33 -0800

    Add completions for bash and zsh

---
+++ tests/integration/completion/mod.rs
@@ -0,0 +1,20 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+
+#[test]
+fn it_generates_bash_completion() {
+    let mut cmd = Command::cargo_bin("rover").unwrap();
+    let result = cmd.args(["completion", "bash"]).assert().success();
+
+    // Bash completion scripts should contain function definitions
+    result.stdout(predicate::str::contains("_Rover"));
+}
+
+#[test]
+fn it_generates_zsh_completion() {
+    let mut cmd = Command::cargo_bin("rover").unwrap();
+    let result = cmd.args(["completion", "zsh"]).assert().success();
+
+    // Zsh completion scripts should contain completion function definitions
+    result.stdout(predicate::str::contains("_Rover"));
+}
