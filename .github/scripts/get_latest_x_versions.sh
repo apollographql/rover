55f891e812908a96f6a8ac3480a5bdb0dd8cddf3
Dylan Anthony <dylan@apollographql.com>
2025-11-14 10:10:59 -0700

    ci: Simplify e2e tests (#2862)
    1. Reduce the amount of plugins that are tested on a schedule. This
    should reduce the flakiness significantly, since it seems the most
    common flake is failure to fetch plugins (guessing rate limiting)
    2. By default, only test the latest versions for each tag from our
    `latest_plugin_versions.json` instead of also fetching additional
    versions from GitHub. That just simplifies the script a lot, and there
    wasn't a clear reason we'd test the last N versions.
    3. Remove installing/running `semver` from npm (we just don't need it
    now, we know the format of plugins)
    4. Combine the manual workflow with the scheduled workflow so it's
    easier to test the scheduled one (and reduce more boilerplate)

--- .github/scripts/get_latest_x_versions.sh
+++ .github/scripts/get_latest_x_versions.sh
@@ -1,49 +1,37 @@
 #!/opt/homebrew/bin/bash
 set -euo pipefail
 
-VERSION_COUNT=$(($1))
-GITHUB_ORG=$2
-GITHUB_REPO=$3
-COMPONENT=$4
-VERSION_TAG=$5
-MAJOR_VERSION=$6
+if [ "$#" -lt 2 ]; then
+  >&2 echo "Usage: $0 COMPONENT VERSION_TAG [VERSION_TAG ...]"
+  exit 1
+fi
 
->&2 echo "VERSION_COUNT is $VERSION_COUNT"
->&2 echo "GITHUB_ORG is $GITHUB_ORG"
->&2 echo "GITHUB_REPO is $GITHUB_REPO"
+COMPONENT=$1
+shift
+VERSION_TAGS=("$@")
+
 >&2 echo "COMPONENT is $COMPONENT"
->&2 echo "VERSION_TAG is $VERSION_TAG"
->&2 echo "MAJOR_VERSION is $MAJOR_VERSION"
+>&2 echo "VERSION_TAGS are ${VERSION_TAGS[*]}"
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 LATEST_PLUGIN_VERSIONS_PATH="$SCRIPT_DIR/../../latest_plugin_versions.json"
-MAX_VERSION=$(jq -r --arg component "$COMPONENT" --arg version_tag "$VERSION_TAG" '.[$component].versions.[$version_tag] | sub("v";"")' < "$LATEST_PLUGIN_VERSIONS_PATH")
->&2 echo "Max Version is: $MAX_VERSION"
 
-MAX_VERSION_FOUND=0
-declare -a FINAL_VERSIONS=()
-VERSIONS_FOUND=0
+declare -a CLEAN_VERSIONS=()
 
-RELEASES_URL="https://api.github.com/repos/$GITHUB_ORG/$GITHUB_REPO/releases?per_page=100"
->&2 echo "Getting releases from $RELEASES_URL"
-RAW_VERSIONS=$(curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "$RELEASES_URL")
-while read -r version; do
-  >&2 echo "Scanning version $version"
-  HIGHEST_VERSION=$(semver -c "$MAX_VERSION" "$version" | tail -1)
-  if [[ $HIGHEST_VERSION == "$MAX_VERSION" ]]; then
-      >&2 echo "Latest Version Found!"
-      MAX_VERSION_FOUND=1
-  fi;
-  if [ $MAX_VERSION_FOUND -eq 1 ]; then
-    if [ $VERSIONS_FOUND -lt $VERSION_COUNT ]; then
-      CLEAN_VERSION=$(semver -c "$version")
-      >&2 echo "Adding $CLEAN_VERSION to final list"
-      FINAL_VERSIONS+=("$CLEAN_VERSION")
-      (( VERSIONS_FOUND+=1 ))
-    else
-      break
-    fi
-  fi;
-done < <(echo "$RAW_VERSIONS" | jq -rc --arg major_version "v$MAJOR_VERSION" '[.[] | select((.name | contains("-") | not ) and (.name | contains($major_version)))] | .[].name  | sub("v";"")')
+for VERSION_TAG in "${VERSION_TAGS[@]}"; do
+  LATEST_VERSION=$(jq -er --arg component "$COMPONENT" --arg version_tag "$VERSION_TAG" '.[$component].versions[$version_tag]' "$LATEST_PLUGIN_VERSIONS_PATH") || {
+    >&2 echo "Unable to read latest version for component '$COMPONENT' with tag '$VERSION_TAG'"
+    exit 1
+  }
 
-jq -c -n '$ARGS.positional' --args -- "${FINAL_VERSIONS[@]}"
+  if [ -z "$LATEST_VERSION" ] || [ "$LATEST_VERSION" = "null" ]; then
+    >&2 echo "No entry found for component '$COMPONENT' with tag '$VERSION_TAG' in $LATEST_PLUGIN_VERSIONS_PATH"
+    exit 1
+  fi
+
+  CLEAN_VERSION="${LATEST_VERSION#v}"
+  >&2 echo "Latest version pulled from manifest for tag '$VERSION_TAG': $CLEAN_VERSION"
+  CLEAN_VERSIONS+=("$CLEAN_VERSION")
+done
+
+jq -c -n '$ARGS.positional' --args -- "${CLEAN_VERSIONS[@]}"
