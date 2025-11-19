3cdeeec47528978be30e660f24d8ef86d8a0cdc8
Julia Naomi <jnboeira@outlook.com>
2025-11-19 15:04:40 -0500

    test

---
+++ regressions/src/lib.rs
@@ -0,0 +1,12 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn connectors_cli_tests() {
+        trycmd::TestCases::new().case("e2e/connectors/*.md");
+    }
+
+    #[test]
+    fn introspection_cli_tests() {
+        trycmd::TestCases::new().case("e2e/introspection/*.md");
+    }
+}
--- regressions/tests/lib.rs
+++
@@ -1,4 +0,0 @@
-#[test]
-fn cli_tests() {
-    trycmd::TestCases::new().case("e2e/**/*.md");
-}
