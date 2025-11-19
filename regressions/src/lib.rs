72f67d6b406ac080267e854baad367faf8cb8b56
Julia Naomi <jnboeira@outlook.com>
2025-11-19 12:28:22 -0500

    use mise

--- regressions/src/lib.rs
+++ regressions/src/lib.rs
@@ -1,11 +1,9 @@
 #[test]
-#[ignore]
 fn connectors_cli_tests() {
     trycmd::TestCases::new().case("e2e/connectors/*.md");
 }
 
 #[test]
-#[ignore]
 fn introspection_cli_tests() {
     trycmd::TestCases::new().case("e2e/introspection/*.md");
 }
