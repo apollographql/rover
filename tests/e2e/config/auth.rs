4e8071ad46203424455bb6a46c1c7908bb12a988
Shane Myrick <shane@apollographql.com>
2025-11-26 10:41:58 -0800

    Fix clippy warniings

--- tests/e2e/config/auth.rs
+++ tests/e2e/config/auth.rs
@@ -1,11 +1,11 @@
-use assert_cmd::Command;
+use assert_cmd::cargo::cargo_bin_cmd;
 use predicates::prelude::*;
 use rstest::rstest;
 
 #[rstest]
 #[ignore]
 fn e2e_test_rover_auth_help() {
-    let mut cmd = Command::cargo_bin("rover").unwrap();
+    let mut cmd = cargo_bin_cmd!("rover");
     cmd.arg("config")
         .arg("auth")
         .arg("--help")
@@ -16,7 +16,7 @@ fn e2e_test_rover_auth_help() {
 #[rstest]
 #[ignore]
 fn e2e_test_rover_auth_fail_empty_api_key() {
-    let mut cmd = Command::cargo_bin("rover").unwrap();
+    let mut cmd = cargo_bin_cmd!("rover");
     let result = cmd.arg("config").arg("auth").write_stdin("").assert();
     result.stderr(predicate::str::contains("empty"));
 }
