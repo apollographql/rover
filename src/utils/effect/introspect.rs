5b7aab6de1f6640f107af154ba379db405e02441
Isaac M. Good <imgood@apollographql.com>
2026-02-19 16:31:03 -0800

    Fix one last remaining non-E2E test

--- src/utils/effect/introspect.rs
+++ src/utils/effect/introspect.rs
@@ -77,6 +77,14 @@ mod test {
 
     #[fixture]
     #[once]
+    pub fn install_crypto_provider() {
+        rustls::crypto::ring::default_provider()
+            .install_default()
+            .expect("Failed to install rustls crypto provider");
+    }
+
+    #[fixture]
+    #[once]
     fn query() -> &'static str {
         SUBGRAPH_INTROSPECTION_QUERY
     }
@@ -84,7 +92,10 @@ mod test {
     #[rstest]
     #[timeout(Duration::from_secs(1))]
     #[tokio::test]
-    async fn test_introspect_subgraph_success(query: &str) -> Result<()> {
+    async fn test_introspect_subgraph_success(
+        query: &str,
+        _install_crypto_provider: (),
+    ) -> Result<()> {
         let server = MockServer::start_async().await;
         server.mock(|when, then| {
             let expected_body = json!({
