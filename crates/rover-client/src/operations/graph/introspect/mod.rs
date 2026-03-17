83a133ec6898e87f62f47cb90130565295429e41
Brian George <brian@apollographql.com>
2026-03-17 13:00:23 -0400

    Refactor

--- crates/rover-client/src/operations/graph/introspect/mod.rs
+++ crates/rover-client/src/operations/graph/introspect/mod.rs
@@ -1,7 +1,9 @@
 mod runner;
 mod schema;
+mod service;
 mod types;
 
 pub use runner::run;
 pub use schema::Schema;
+pub use service::{GraphIntrospect, GraphIntrospectError};
 pub use types::{GraphIntrospectInput, GraphIntrospectResponse};
--- crates/rover-client/src/operations/graph/introspect/runner.rs
+++ crates/rover-client/src/operations/graph/introspect/runner.rs
@@ -1,25 +1,12 @@
-use std::convert::{Into, TryFrom};
-
-use graphql_client::GraphQLQuery;
 use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
 
 use crate::{
     blocking::GraphQLClient,
     error::{EndpointKind, RoverClientError},
-    operations::graph::introspect::{types::*, Schema},
+    operations::graph::introspect::types::*,
 };
 
-#[derive(GraphQLQuery)]
-#[graphql(
-    query_path = "src/operations/graph/introspect/introspect_query.graphql",
-    schema_path = "src/operations/graph/introspect/introspect_schema.graphql",
-    response_derives = "PartialEq, Eq, Debug, Serialize, Deserialize",
-    deprecated = "warn"
-)]
-/// This struct is used to generate the module containing `Variables` and
-/// `ResponseData` structs.
-/// Snake case of this name is the mod name. i.e. graph_introspect_query
-pub(crate) struct GraphIntrospectQuery;
+use super::service::GraphIntrospectQuery;
 
 /// The main function to be used from this module. This function fetches a
 /// schema from apollo studio and returns it in either sdl (default) or json format
@@ -50,16 +37,5 @@ pub async fn run(
             .await
     }?;
 
-    build_response(response_data)
-}
-
-fn build_response(
-    response: QueryResponseData,
-) -> Result<GraphIntrospectResponse, RoverClientError> {
-    match Schema::try_from(response) {
-        Ok(schema) => Ok(GraphIntrospectResponse {
-            schema_sdl: schema.encode(),
-        }),
-        Err(msg) => Err(RoverClientError::IntrospectionError { msg: msg.into() }),
-    }
+    super::service::build_response(response_data).map_err(RoverClientError::from)
 }
