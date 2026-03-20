f9fc132db066ffbf61924d40c4788704d699b49b
Brian George <brian@apollographql.com>
2026-03-20 12:37:17 -0400

    Remove unneeded tower service changes

--- crates/rover-client/src/operations/graph/introspect/mod.rs
+++ crates/rover-client/src/operations/graph/introspect/mod.rs
@@ -1,9 +1,7 @@
 mod runner;
 mod schema;
-mod service;
 mod types;
 
 pub use runner::run;
 pub use schema::Schema;
-pub use service::{GraphIntrospect, GraphIntrospectError};
 pub use types::{GraphIntrospectInput, GraphIntrospectResponse};
