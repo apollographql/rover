2f21dfbbbb313eec257b89e2f86e0abfeccb8235
Brian George <brian@apollographql.com>
2026-03-20 12:37:17 -0400

    Remove unneeded tower service changes

--- crates/rover-client/src/operations/supergraph/fetch/mod.rs
+++ crates/rover-client/src/operations/supergraph/fetch/mod.rs
@@ -1,7 +1,5 @@
 mod runner;
-mod service;
 mod types;
 
 pub use runner::run;
-pub use service::{SupergraphFetch, SupergraphFetchRequest};
 pub use types::SupergraphFetchInput;
--- crates/rover-client/src/operations/supergraph/fetch/types.rs
+++ crates/rover-client/src/operations/supergraph/fetch/types.rs
@@ -1,14 +1,17 @@
 use rover_studio::types::GraphRef;
 
-use super::service::SupergraphFetchRequest;
+use crate::operations::supergraph::fetch::runner::supergraph_fetch_query;
+
+type QueryVariables = supergraph_fetch_query::Variables;
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct SupergraphFetchInput {
     pub graph_ref: GraphRef,
 }
 
-impl From<SupergraphFetchInput> for SupergraphFetchRequest {
+impl From<SupergraphFetchInput> for QueryVariables {
     fn from(input: SupergraphFetchInput) -> Self {
-        Self::from(input.graph_ref)
+        let (graph_id, variant) = input.graph_ref.into_parts();
+        Self { graph_id, variant }
     }
 }
